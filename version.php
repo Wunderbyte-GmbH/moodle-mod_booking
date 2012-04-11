3c55570ec29623426f491a1c336adc58ff71a6e2
David Bogner <info@edulabs.org>
2012-04-11 15:26:42 +0200

    Version info adapted to moodle requirements

--- version.php
+++ version.php
@@ -5,8 +5,16 @@
 //  This fragment is called by /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
+/**
+ * @package block_course_contents
+ * @copyright 2012 David Bogner <info@edulabs.org>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 $module->version  = 2011020402;
 $module->requires = 2010112400;  // Requires this Moodle 2.X version
+$module->release = '2.1.0';
+$module->maturity = MATURITY_STABLE;
 $module->cron     = 0;
 
 ?>
