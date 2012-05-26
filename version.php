75f7c70396fd650726605e1b3004e5ec6d26cddb
David Bogner <info@edulabs.org>
2012-05-27 01:10:26 +0200

    updated version

--- version.php
+++ version.php
@@ -11,9 +11,9 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version  = 2012041201;
+$module->version  = 2012052501;
 $module->requires = 2010112400;  // Requires this Moodle 2.X version
-$module->release = '2.2.0';
+$module->release = '2.3.0';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 0;
 
