6dad087754ddffeee4a0034355ae1c6e3e498b36
David Bogner <info@edulabs.org>
2012-07-17 22:13:16 +0200

    added capability addinstance

--- db/access.php
+++ db/access.php
@@ -83,7 +83,19 @@ $capabilities = array(
             'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
         )
-    )
+    ),		
+
+		'mod/booking:addinstance' => array(
+				'riskbitmask' => RISK_XSS,
+		
+				'captype' => 'write',
+				'contextlevel' => CONTEXT_COURSE,
+				'archetypes' => array(
+						'editingteacher' => CAP_ALLOW,
+						'manager' => CAP_ALLOW
+				),
+				'clonepermissionsfrom' => 'moodle/course:manageactivities'
+		),
 );
 
 ?>
--- version.php
+++ version.php
@@ -11,7 +11,7 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version  = 2012052901;
+$module->version  = 2012071701;
 $module->requires = 2010112400;  // Requires this Moodle 2.X version
 $module->release = '2.3.1';
 $module->maturity = MATURITY_STABLE;
