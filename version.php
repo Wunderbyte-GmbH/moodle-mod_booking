34ca4d54fd14ead819bf792a87be8bd0fafa6412
David Bogner <info@edulabs.org>
2013-03-11 10:36:05 +0100

    version number updated

--- version.php
+++ version.php
@@ -6,12 +6,12 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 /**
- * @package block_course_contents
- * @copyright 2012 David Bogner <info@edulabs.org>
+ * @package mod_booking
+ * @copyright 2012,2013 David Bogner <info@edulabs.org>
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version  = 2012111700;
+$module->version  = 2013031100;
 $module->requires = 2010112400;  // Requires this Moodle 2.X version
 $module->release = '2.3.1';
 $module->maturity = MATURITY_STABLE;
