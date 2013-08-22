391d0a79949d3aa30b50c2e04e4ebdedf45c9bc5
David Bogner <davidbogner@gmail.com>
2013-08-22 19:18:40 +0200

    added bookingpolicy agree for booking other users

--- version.php
+++ version.php
@@ -11,8 +11,8 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version  = 2013081802;
+$module->version  = 2013082200;
 $module->requires = 2010112400;  // Requires this Moodle 2.X version
-$module->release = '2.3.3';
+$module->release = '2.3.4';
 $module->maturity = MATURITY_STABLE;
 $module->cron     = 0;
