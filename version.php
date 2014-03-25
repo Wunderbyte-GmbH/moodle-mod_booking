8dc05fa1b9d08802cb135f19085402111d9a01f9
Andraž <atletek@gmail.com>
2014-03-25 09:31:59 +0100

    Updated cron

--- version.php
+++ version.php
@@ -11,8 +11,8 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version  = 2014032102;
+$module->version  = 2014032500;
 $module->requires = 2010112400;  // Requires this Moodle 2.X version
 $module->release = '2.5';
 $module->maturity = MATURITY_STABLE;
-$module->cron     = 1;
+$module->cron     = 5;
