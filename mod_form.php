6933808e81b197ec39f8580fd3c3c02a29f2bd08
Andraž <atletek@gmail.com>
2014-04-07 09:36:37 +0000

    Search tags text belog tags element.

--- mod_form.php
+++ mod_form.php
@@ -196,10 +196,11 @@ class mod_booking_mod_form extends moodleform_mod {
         $mform->addHelpButton('maxperuser', 'maxperuser', 'mod_booking');
 
         $mform->addElement('header', 'tagsheader', get_string('tags'));
-        $mform->addElement('tags', 'tags', get_string('tags'));
 
         $taglink = new moodle_url('/tag/search.php', array());
         $mform->addElement('html', '<a target="_blank" href="' . $taglink . '">' . get_string('searchtag', 'booking') . '</a>');
+        $mform->addElement('static', 'searchtag', '', '<a target="_blank" href="' . $taglink . '">' . get_string('searchtag', 'booking') . '</a>');
+        $mform->addElement('tags', 'tags', get_string('tags'));
 
         $options = array();
 
