67be4d86bf48046261b39a2b6c6dedcbc3e8e6fa
Jarosław Maciejewski <nitro.bystrzyca@gmail.com>
2015-11-19 01:53:57 +0100

    fix version for Moodle 3.0

--- version.php
+++ version.php
@@ -1,17 +1,7 @@
 <?php
-
-////////////////////////////////////////////////////////////////////////////////
-//  Code fragment to define the module version etc.
-//  This fragment is called by /admin/index.php
-////////////////////////////////////////////////////////////////////////////////
-
-/**
- * @package mod_booking
- * @copyright 2012,2013,2014,2015 David Bogner <info@edulabs.org>, Andraž Prinčič <atletek@gmail.com>
- * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
-$module->version = 2015082600;
-$module->requires = 2013111800;  // Requires this Moodle 2.X version
-$module->release = '2.7';
-$module->maturity = MATURITY_STABLE;
-$module->cron = 5;
+$plugin->version = 2015082600;
+$plugin->requires = 2013111800; // Requires this Moodle 2.X version
+$plugin->release = '2.7';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->cron = 5;
+$plugin->component = 'mod_booking';
