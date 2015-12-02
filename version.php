79b0e7c19faf7aff59ab9f4e089eda21ce1caa6e
David Bogner <davidbogner@gmail.com>
2015-12-02 13:33:26 +0100

    Component added to version.php

--- version.php
+++ version.php
@@ -15,3 +15,4 @@ $plugin->requires = 2013111800;  // Requires this Moodle 2.X version
 $plugin->release = '2.7';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 5;
+$plugin->component = 'mod_boking';
