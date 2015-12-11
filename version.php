29c29a8cea32562215b4ec224f7be86653391e80
375michael40veit <m-veit@web.de>
2015-12-11 13:19:40 +0100

    Updated for moodle 2.9

--- version.php
+++ version.php
@@ -10,9 +10,9 @@
  * @copyright 2012,2013,2014,2015 David Bogner <info@edulabs.org>, Andraž Prinčič <atletek@gmail.com>
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-$plugin->version = 2015120100;
+$plugin->version = 2015120101;
 $plugin->requires = 2013111800;  // Requires this Moodle 2.X version
-$plugin->release = '2.7';
+$plugin->release = '2.9';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 5;
 $plugin->component = 'mod_booking';
