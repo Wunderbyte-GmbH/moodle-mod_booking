e268e2f8ec91639cfc51476c8b25789e17a5279d
Andraž <atletek@gmail.com>
2016-12-12 21:31:12 +0100

    Bug fix - added DISTINCT - was returning too much rows - subquery!

--- version.php
+++ version.php
@@ -14,7 +14,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version =  2016122300;
 $plugin->requires = 2014051200;  // Requires this Moodle 2.X version
-$plugin->release = 'Eva Thörnblad 2.3'; // famous female characters: Diane Selwyn, Eva Thörnblad,
+$plugin->release = 'Eva Thörnblad 2.4'; // famous female characters: Diane Selwyn, Eva Thörnblad,
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 5;
 $plugin->component = 'mod_booking';
