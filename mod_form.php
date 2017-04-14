3ed5c36c1f5cfdb18b2239358ba9843d799f1827
Andraž <atletek@gmail.com>
2017-04-14 10:56:18 +0200

    Travis-ci fixes - round 3.

--- mod_form.php
+++ mod_form.php
@@ -182,7 +182,7 @@ class mod_booking_mod_form extends moodleform_mod {
         $mform->setType('daystonotify', PARAM_INT);
         $mform->setDefault('daystonotify', 0);
         $mform->addHelpButton('daystonotify', 'daystonotify', 'booking');
-        
+
         $mform->addElement('text', 'daystonotify2', get_string('daystonotify2', 'booking'));
         $mform->setType('daystonotify2', PARAM_INT);
         $mform->setDefault('daystonotify2', 0);
