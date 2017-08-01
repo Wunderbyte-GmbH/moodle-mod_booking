adc1e6ca8eba449729d38b714a1c1a2f419ab869
atlet <atletek@gmail.com>
2017-08-01 17:14:37 +0200

    Added 2 new events to log add and remove teachers from option. They are

---
+++ classes/event/teacher_added.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * The mod_booking taecher added event.
+ *
+ * @package mod_booking
+ * @copyright 2014 David Bogner http://www.edulabs.org
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace mod_booking\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+
+/**
+ * The mod_booking report viewed event class.
+ *
+ * @package mod_booking
+ * @since Moodle 2.7
+ * @copyright 2014 David Bogner
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class teacher_added extends \core\event\base {
+	
+	/**
+	 * Init method.
+	 */
+	protected function init() {
+		$this->data['crud'] = 'c';
+		$this->data['edulevel'] = self::LEVEL_TEACHING;
+		$this->data['objecttable'] = 'booking_teachers';
+	}
+	
+	/**
+	 * Returns localised general event name.
+	 *
+	 * @return string
+	 */
+	public static function get_name() {
+		return get_string('eventteacher_added', 'mod_booking');
+	}
+	
+	public function get_description() {
+		return "The user with id '$this->userid' added teacher with user id '$this->relateduserid' to the booking option with the option id
+        '$this->objectid'";
+    }
+
+    /**
+     * Returns relevant URL.
+     *
+     * @return \moodle_url
+     */
+    public function get_url() {
+        return new \moodle_url('/mod/booking/teachers.php',
+        		array('id' => $this->contextinstanceid, 'optionid' => $this->objectid));
+    }
+}
---
+++ tests/events_test.php
@@ -0,0 +1,187 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for booking events.
+ *
+ * @package    mod_booking
+ * @category   test
+ * @copyright  2017 Andraž Prinčič <atletek@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Tests for forum events.
+ *
+ * @package    mod_forum
+ * @category   test
+ * @copyright  2014 Dan Poltawski <dan@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mod_booking_events_testcase extends advanced_testcase {
+	
+	/**
+	 * Tests set up.
+	 */
+	public function setUp() {		
+		$this->resetAfterTest();
+	}
+	
+	public function tearDown() {
+	}
+	
+	/**
+	 * Test teacher_added event.
+	 */
+	public function test_teacher_added() {
+		
+		// Setup test data.
+		$course = $this->getDataGenerator()->create_course();				
+		$user1 = $this->getDataGenerator()->create_user();
+		$user2 = $this->getDataGenerator()->create_user();
+		
+		$bdata = array(
+				'course' => $course->id,
+				'name' => 'Test Booking', 
+				'eventtype' => 'Test event', 
+				'bookingmanager' => $user2->username,
+				'bookedtext' => array('text' => 'text'),
+				'waitingtext' => array('text' => 'text'),
+				'notifyemail' => array('text' => 'text'),
+				'statuschangetext' => array('text' => 'text'),
+				'deletedtext' => array('text' => 'text'),
+				'pollurltext' => array('text' => 'text'),
+				'pollurlteacherstext' => array('text' => 'text'),
+				'notificationtext' => array('text' => 'text'),
+				'userleave' => array('text' => 'text'),
+				'bookingpolicy' => 'bookingpolicy',
+				'tags' => ''
+		);
+		
+		$booking = $this->getDataGenerator()->create_module('booking', $bdata);	
+		
+		$this->setUser($user2);
+		$this->setAdminUser();
+		
+		$this->getDataGenerator()->enrol_user($user1->id, $course->id);
+		$this->getDataGenerator()->enrol_user($user2->id, $course->id);
+		
+		$coursectx = context_course::instance($course->id);
+		
+		$record = new stdClass();
+		$record->bookingid = $booking->id;
+		$record->text = 'Test option';
+		$record->courseid = $course->id;
+		$record->description = 'Test description';
+		
+		$option = self::getDataGenerator()->get_plugin_generator('mod_booking')->create_option($record);		
+		
+		$params = array(
+				'relateduserid' => $user1->id, 
+				'objectid' => $option->id, 
+				'context' => $coursectx
+		);
+		
+		// Create event.
+		$event = \mod_booking\event\teacher_added::create($params);
+		
+		// Trigger and capture the event.
+		$sink = $this->redirectEvents();
+		$event->trigger();
+		$events = $sink->get_events();
+		$this->assertCount(1, $events);
+		$event = reset($events);
+		
+		// Checking that the event contains the expected values.
+		$this->assertInstanceOf('\mod_booking\event\teacher_added', $event);
+		$this->assertEquals($coursectx, $event->get_context());
+		$this->assertEventContextNotUsed($event);
+		
+		$this->assertNotEmpty($event->get_name());
+	}
+	
+	/**
+	 * Test teacher_removed event.
+	 */
+	public function test_teacher_removed() {
+		
+		// Setup test data.
+		$course = $this->getDataGenerator()->create_course();
+		$user1 = $this->getDataGenerator()->create_user();
+		$user2 = $this->getDataGenerator()->create_user();
+		
+		$bdata = array(
+				'course' => $course->id,
+				'name' => 'Test Booking',
+				'eventtype' => 'Test event',
+				'bookingmanager' => $user2->username,
+				'bookedtext' => array('text' => 'text'),
+				'waitingtext' => array('text' => 'text'),
+				'notifyemail' => array('text' => 'text'),
+				'statuschangetext' => array('text' => 'text'),
+				'deletedtext' => array('text' => 'text'),
+				'pollurltext' => array('text' => 'text'),
+				'pollurlteacherstext' => array('text' => 'text'),
+				'notificationtext' => array('text' => 'text'),
+				'userleave' => array('text' => 'text'),
+				'bookingpolicy' => 'bookingpolicy',
+				'tags' => ''
+		);
+		
+		$booking = $this->getDataGenerator()->create_module('booking', $bdata);
+		
+		$this->setUser($user2);
+		$this->setAdminUser();
+		
+		$this->getDataGenerator()->enrol_user($user1->id, $course->id);
+		$this->getDataGenerator()->enrol_user($user2->id, $course->id);
+		
+		$coursectx = context_course::instance($course->id);
+		
+		$record = new stdClass();
+		$record->bookingid = $booking->id;
+		$record->text = 'Test option';
+		$record->courseid = $course->id;
+		$record->description = 'Test description';
+		
+		$option = self::getDataGenerator()->get_plugin_generator('mod_booking')->create_option($record);
+		
+		$params = array(
+				'relateduserid' => $user1->id,
+				'objectid' => $option->id,
+				'context' => $coursectx
+		);
+		
+		// Create event.
+		$event = \mod_booking\event\teacher_removed::create($params);
+		
+		// Trigger and capture the event.
+		$sink = $this->redirectEvents();
+		$event->trigger();
+		$events = $sink->get_events();
+		$this->assertCount(1, $events);
+		$event = reset($events);
+		
+		// Checking that the event contains the expected values.
+		$this->assertInstanceOf('\mod_booking\event\teacher_removed', $event);
+		$this->assertEquals($coursectx, $event->get_context());
+		$this->assertEventContextNotUsed($event);
+		
+		$this->assertNotEmpty($event->get_name());
+	}
+}
