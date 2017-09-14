c8025985fee00a157b3943b78a17bdfb9f1ea34a
atlet <atletek@gmail.com>
2017-09-14 18:01:57 +0200

    TravisCI

---
+++ rating_rest.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+if (!defined('AJAX_SCRIPT')) {
+    define('AJAX_SCRIPT', true);
+}
+
+require_once (__DIR__ . '/../../config.php');
+require_once ($CFG->dirroot . '/mod/booking/locallib.php');
+
+$id = required_param('id', PARAM_INT); // Course Module ID
+$optionid = required_param('optionid', PARAM_INT); // Course Module ID
+$value = required_param('value', PARAM_INT); // Course Module ID
+
+list($course, $cm) = get_course_and_cm_from_cmid($id, 'booking');
+require_course_login($course, false, $cm);
+
+echo $OUTPUT->header();
+
+$record = new stdClass();
+$record->userid = $USER->id;
+$record->optionid = $optionid;
+$record->rate = $value;
+
+try {
+    $DB->insert_record('booking_ratings', $record, false, false);
+} catch (Exception $e) {
+    //
+}
+
+$avg = $DB->get_record_sql(
+        'SELECT IFNULL(AVG(rate), 1) AS rate
+        FROM {booking_ratings}
+        WHERE optionid = ?', array($optionid));
+
+echo json_encode(array('rate' => (int) $avg->rate));
