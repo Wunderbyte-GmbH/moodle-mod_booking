97448ed3b44a5c10da415f86ea1a808666ea2649
David Bogner <davidbogner@gmail.com>
2019-05-27 14:47:31 +0200

    Fix autocomplete form validation for Moodle 3.5.6+

--- version.php
+++ version.php
@@ -22,11 +22,11 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019050400;
+$plugin->version = 2019052700;
 $plugin->requires = 2017111300; // Requires this Moodle version. Current: Moodle 3.4.
 // Famous female characters: Diane Selwyn, Eva Thörnblad, Alex Kirkman, Piper Chapman.
 // Lois Wilkerson, Audrey Horne, Lorelai Gilmore.
-$plugin->release = '4.2 Lorelai Gilmore';
+$plugin->release = '4.3 Lorelai Gilmore';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 5;
 $plugin->component = 'mod_booking';
