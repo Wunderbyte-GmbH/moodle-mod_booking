09ed65deea09d993f36bbb76ce239a08aaeaa340
David Bogner <davidbogner@gmail.com>
2019-12-20 10:48:05 +0100

    Minor appearance fix

--- report.php
+++ report.php
@@ -608,7 +608,7 @@ if (!$tableallbookings->is_downloading()) {
              ($bookingdata->option->coursestarttime == 0 ? get_string('nodateset', 'booking') : userdate(
                     $bookingdata->option->coursestarttime, get_string('strftimedatetime')) . " - " .
              userdate($bookingdata->option->courseendtime, get_string('strftimedatetime'))) . " | " .
-             (empty($bookingdata->booking->settings->lblteachname) ? get_string('teachers', 'booking') . ': ' : $bookingdata->booking->settings->lblteachname) .
+             (empty($bookingdata->booking->settings->lblteachname) ? get_string('teachers', 'booking') . ': ' : $bookingdata->booking->settings->lblteachname . ': ') .
              implode(', ', $teachers) . " {$linkst}</p>";
 
     $links = array();
