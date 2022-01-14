c950c5d76acb3628ea8cc5b4d795432554683538
Andraž P <atletek@gmail.com>
2022-01-14 08:37:37 +0100

    Added missing field to backup.

--- backup/moodle2/backup_booking_stepslib.php
+++ backup/moodle2/backup_booking_stepslib.php
@@ -51,7 +51,7 @@ class backup_booking_activity_structure_step extends backup_activity_structure_s
                     'signinsheetfields', 'comments', 'ratings', 'removeuseronunenrol', 'teacherroleid', 'allowupdatedays',
                     'templateid', 'showdescriptionmode', 'showlistoncoursepage', 'coursepageshortinfo', 'bookingimagescustomfield',
                     'defaultoptionsort', 'showviews', 'customtemplateid', 'autcractive', 'autcrprofile', 'autcrvalue',
-                    'autcrtemplate'));
+                    'autcrtemplate', 'maxperuseronlyactive'));
 
         $options = new backup_nested_element('options');
         $option = new backup_nested_element('option', array('id'),
