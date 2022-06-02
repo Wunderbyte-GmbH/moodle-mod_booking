39023ad947ac966ae109400c171193278ff6aa33
georgmaisser <georg.maisser@wunderbyte.at>
2022-06-02 09:03:36 +0200

    Version 7.1.4 (2022060200)

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022060101;
+$plugin->version = 2022060200;
 $plugin->requires = 2019111800; // Requires this Moodle version. Current: Moodle 3.8.
-$plugin->release = 'v7.1.3';
+$plugin->release = 'v7.1.4';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'mod_booking';
