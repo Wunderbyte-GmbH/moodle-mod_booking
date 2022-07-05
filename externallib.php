ee821f7442a7a97d0e37c96e53ef3c236250d855
Brad Pasley <33439538+bradpasley@users.noreply.github.com>
2022-07-06 07:47:15 +1000

    fix externallib unittesting fail (#196)

--- externallib.php
+++ externallib.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 defined('MOODLE_INTERNAL') || die();
-ini_set('max_execution_time', 600);
+core_php_time_limit::raise(600);
 
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->libdir . "/externallib.php");
