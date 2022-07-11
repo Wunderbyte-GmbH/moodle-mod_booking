5d1bde04bbf8fc80c22989b8a7e4cef393d942ca
georgmaisser <georg.maisser@wunderbyte.at>
2022-07-11 20:36:19 +0200

    Verison (2022071100)

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022070500;
+$plugin->version = 2022071100;
 $plugin->requires = 2022041900; // Requires this Moodle version. Current: Moodle 4.0.
 $plugin->release = 'v7.2.3';
 $plugin->maturity = MATURITY_STABLE;
