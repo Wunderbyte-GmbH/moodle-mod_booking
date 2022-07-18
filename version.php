c7ca787e9007dce8bf2bb7363061138bc35da2a1
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-07-18 11:51:52 +0200

    Setup for xmldb field for elective.

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022071801;
-$plugin->requires = 2021051700; // Requires this Moodle version. Current: Moodle 3.11.
-$plugin->release = 'v7.2.5';
+$plugin->version = 2022071803;
+$plugin->requires = 2019111800; // Requires this Moodle version. Current: Moodle 3.11.
+$plugin->release = 'v7.2.3';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'mod_booking';
