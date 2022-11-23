d5b19d5e0641e05d0054b1c20b25d1d6d702efc3
David Bogner <davidbogner@gmail.com>
2022-11-23 12:12:17 +0100

    Version 7.7.3 (2022112300)

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022112100;
+$plugin->version = 2022112300;
 $plugin->requires = 2021051700; // Requires this Moodle version. Current: Moodle 3.11.
-$plugin->release = '7.7.2';
+$plugin->release = '7.7.3';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'mod_booking';
