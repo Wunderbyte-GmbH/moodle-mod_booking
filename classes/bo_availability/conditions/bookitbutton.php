2fa9a7277998b14010a337492ba065d7a8e4e954
georgmaisser <georg.maisser@wunderbyte.at>
2022-12-29 14:05:38 +0100

    fix deprecation warnings

--- classes/bo_availability/conditions/bookitbutton.php
+++ classes/bo_availability/conditions/bookitbutton.php
@@ -102,7 +102,7 @@ class bookitbutton implements bo_condition {
      * @return array availability and Information string (for admin) about all restrictions on
      *   this item
      */
-    public function get_description($full = false, booking_option_settings $settings, $userid = null, $not = false):array {
+    public function get_description(booking_option_settings $settings, $userid = null, $full = false, $not = false):array {
 
         $description = '';
 
