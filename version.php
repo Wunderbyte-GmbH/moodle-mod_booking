642384f93aaba24d033a3470a13fcc6a5f43ea31
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2023-01-12 13:46:05 +0100

    Version 7.8.1 (2023011200)

--- version.php
+++ version.php
@@ -23,8 +23,8 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2022122300;
+$plugin->version = 2023011200;
 $plugin->requires = 2021051700; // Requires this Moodle version. Current: Moodle 3.11.
-$plugin->release = '7.8.0';
+$plugin->release = '7.8.1';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'mod_booking';
