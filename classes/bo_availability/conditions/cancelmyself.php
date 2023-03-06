9cd537fb447966dec6d9f132ae360bb178a3d3b4
Georg Maißer <georg.maisser@wunderbyte.at>
2023-03-06 18:27:23 +0100

    Add cancel myself conditoin (unfinished)

---
+++ classes/bo_availability/conditions/cancelmyself.php
@@ -0,0 +1,234 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Already booked condition (item has been booked).
+ *
+ * @package mod_booking
+ * @copyright 2022 Wunderbyte GmbH
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_booking\bo_availability\conditions;
+
+use mod_booking\bo_availability\bo_condition;
+use mod_booking\bo_availability\bo_info;
+use mod_booking\booking_option_settings;
+use mod_booking\singleton_service;
+use MoodleQuickForm;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/mod/booking/lib.php');
+
+/**
+ * Base class for a single bo availability condition.
+ *
+ * All bo condition types must extend this class.
+ *
+ *
+ * @package mod_booking
+ * @copyright 2022 Wunderbyte GmbH
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class cancelmyself implements bo_condition {
+
+    /** @var int $id Standard Conditions have hardcoded ids. */
+    public $id = BO_COND_CANCELMYSELF;
+
+    /**
+     * Needed to see if class can take JSON.
+     * @return bool
+     */
+    public function is_json_compatible(): bool {
+        return false; // Hardcoded condition.
+    }
+
+    /**
+     * Needed to see if it shows up in mform.
+     * @return bool
+     */
+    public function is_shown_in_mform(): bool {
+        return false;
+    }
+
+    /**
+     * Determines whether a particular item is currently available
+     * according to this availability condition.
+     * @param booking_option_settings $settings Item we're checking
+     * @param int $userid User ID to check availability for
+     * @param bool $not Set true if we are inverting the condition
+     * @return bool True if available
+     */
+    public function is_available(booking_option_settings $settings, $userid, $not = false):bool {
+
+        global $DB;
+
+        // This is the return value. Not available to begin with.
+        $isavailable = false;
+
+        // Get the booking answers for this instance.
+        $bookinganswer = singleton_service::get_instance_of_booking_answers($settings);
+
+        $bookinginformation = $bookinganswer->return_all_booking_information($userid);
+        $bosettings = singleton_service::get_instance_of_booking_settings_by_cmid($settings->cmid);
+
+        // If the user is not yet booked we return true.
+        if (!isset($bookinginformation['iambooked'])
+            || $bosettings->cancancelbook != 1) {
+
+            $isavailable = true;
+        }
+
+        // If it's inversed, we inverse.
+        if ($not) {
+            $isavailable = !$isavailable;
+        }
+
+        return $isavailable;
+    }
+
+    /**
+     * The hard block is complementary to the is_available check.
+     * While is_available is used to build eg also the prebooking modals and...
+     * ... introduces eg the booking policy or the subbooking page, the hard block is meant to prevent ...
+     * ... unwanted booking. It's the check just before booking if we really...
+     * ... want the user to book. It will return always return false on subbookings...
+     * ... as they are not necessary, but return true when the booking policy is not yet answered.
+     * Hard block is only checked if is_available already returns false.
+     *
+     * @param booking_option_settings $booking_option_settings
+     * @param integer $userid
+     * @return boolean
+     */
+    public function hard_block(booking_option_settings $settings, $userid):bool {
+        return false;
+    }
+
+    /**
+     * Obtains a string describing this restriction (whether or not
+     * it actually applies). Used to obtain information that is displayed to
+     * students if the activity is not available to them, and for staff to see
+     * what conditions are.
+     *
+     * The $full parameter can be used to distinguish between 'staff' cases
+     * (when displaying all information about the activity) and 'student' cases
+     * (when displaying only conditions they don't meet).
+     *
+     * @param bool $full Set true if this is the 'full information' view
+     * @param booking_option_settings $settings Item we're checking
+     * @param int $userid User ID to check availability for
+     * @param bool $not Set true if we are inverting the condition
+     * @return array availability and Information string (for admin) about all restrictions on
+     *   this item
+     */
+    public function get_description(booking_option_settings $settings, $userid = null, $full = false, $not = false):array {
+
+        $description = '';
+
+        $isavailable = $this->is_available($settings, $userid, $not);
+
+        $description = $this->get_description_string($isavailable, $full);
+
+        return [$isavailable, $description, BO_PREPAGE_NONE, BO_BUTTON_JUSTMYALERT];
+    }
+
+    /**
+     * Only customizable functions need to return their necessary form elements.
+     *
+     * @param MoodleQuickForm $mform
+     * @param int $optionid
+     * @return void
+     */
+    public function add_condition_to_mform(MoodleQuickForm &$mform, int $optionid = 0) {
+        // Do nothing.
+    }
+
+    /**
+     * The page refers to an additional page which a booking option can inject before the booking process.
+     * Not all bo_conditions need to take advantage of this. But eg a condition which requires...
+     * ... the acceptance of a booking policy would render the policy with this function.
+     *
+     * @param integer $optionid
+     * @return array
+     */
+    public function render_page(int $optionid) {
+        return [];
+    }
+
+    /**
+     * Some conditions (like price & bookit) provide a button.
+     * Renders the button, attaches js to the Page footer and returns the html.
+     * Return should look somehow like this.
+     * ['mod_booking/bookit_button', $data];
+     *
+     * @param booking_option_settings $settings
+     * @param int $userid
+     * @param bool $full
+     * @param bool $not
+     * @return array
+     */
+    public function render_button(booking_option_settings $settings,
+        int $userid = 0, bool $full = false, bool $not = false, bool $fullwidth = true): array {
+
+            global $USER;
+
+            if ($userid === null) {
+                $userid = $USER->id;
+            }
+            $label = $this->get_description_string(false, $full);
+
+            if ($fullwidth) {
+                // For view.php and default rendering.
+                $class = 'btn btn-secondary w-100 mt-0 mb-0 pl-1 pr-1 pt-2 pb-2';
+            } else {
+                // For prepage modals we want to render the button different than on view.php.
+                $class = 'btn btn-success pl-3 pr-3 pb-2 pt-2 m-3';
+            }
+
+            return [
+                'mod_booking/bookit_button',
+                [
+                    'itemid' => $settings->id,
+                    'area' => 'option',
+                    'userid' => $userid ?? 0,
+                    'main' => [
+                        'label' => $label,
+                        'class' => $class,
+                        'role' => 'button',
+                    ]
+                ]
+            ];
+    }
+
+    /**
+     * Helper function to return localized description strings.
+     *
+     * @param bool $isavailable
+     * @param bool $full
+     * @return string
+     */
+    private function get_description_string($isavailable, $full) {
+        if ($isavailable) {
+            $description = $full ? get_string('bo_cond_alreadybooked_full_available', 'mod_booking') :
+                get_string('bo_cond_alreadybooked_available', 'mod_booking');
+        } else {
+            $description = $full ? get_string('bo_cond_alreadybooked_full_not_available', 'mod_booking') :
+                get_string('bo_cond_alreadybooked_not_available', 'mod_booking');
+        }
+        return 'du darfst stornieren';
+    }
+}
