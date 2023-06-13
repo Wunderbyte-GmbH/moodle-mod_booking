77d41d4efe43a24464e79951ed2eb8db8e7ca489
Georg Maißer <georg.maisser@wunderbyte.at>
2023-06-13 16:24:03 +0200

    Fix error when not an elective.

--- lib.php
+++ lib.php
@@ -1245,8 +1245,10 @@ function booking_update_options($optionvalues, $context) {
 
         // Elective.
         // Save combination arrays to DB.
-        elective::addcombinations($optionid, $optionvalues->mustcombine, 1);
-        elective::addcombinations($optionid, $optionvalues->mustnotcombine, 0);
+        if (!empty($booking->iselective)) {
+            elective::addcombinations($optionid, $optionvalues->mustcombine, 1);
+            elective::addcombinations($optionid, $optionvalues->mustnotcombine, 0);
+        }
 
         $option->credits = $optionvalues->credits ?? 0;
         $option->sortorder = $optionvalues->sortorder ?? 0;
