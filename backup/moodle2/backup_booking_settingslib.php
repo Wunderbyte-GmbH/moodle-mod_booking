d6f9d1d02e1dcddc143cb7882e6164c46f2754a8
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2023-06-14 11:09:21 +0200

    Linting.

--- backup/moodle2/backup_booking_settingslib.php
+++ backup/moodle2/backup_booking_settingslib.php
@@ -22,4 +22,8 @@
  * @copyright 2012 onwards David Bogner
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-require_once(__DIR__ . '/../../../../config.php');
+// phpcs:ignore Squiz.PHP.CommentedOutCode.Found
+/*require_once(__DIR__ . '/../../../../config.php');
+
+// No guest autologin.
+require_login(0, false);*/
