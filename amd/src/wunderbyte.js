40a08e606549fbc869af37612af32f22d71b350c
Andrii Semenets <semteacher@gmail.com>
2023-06-18 17:14:51 +0300

    GitHub Grunt: suppress "Unexpected empty function 'WunderByteJS'" in the wunderbyte.js (Moodle had been updated as well to achieve this)

--- amd/src/wunderbyte.js
+++ amd/src/wunderbyte.js
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/* eslint-disable no-empty-function */
+
 /**
  * WunderByte javascript library/framework
  *
