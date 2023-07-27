8875bbbcf7656595a846966f8cd78bd153af7531
Georg Maißer <georg.maisser@wunderbyte.at>
2023-07-27 16:41:04 +0200

    Fix problem with shortcode table

--- classes/shortcodes.php
+++ classes/shortcodes.php
@@ -63,7 +63,7 @@ class shortcodes {
             $perpage = 1000;
         }
 
-        $table = self::init_table_for_courses($course->shortname);
+        $table = self::init_table_for_courses(null, $course->shortname);
 
         $wherearray['recommendedin'] = "%$course->shortname%";
 
