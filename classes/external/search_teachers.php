fbdab6ddbba43bb720f472af650260d1392c5b16
Georg Maißer <georg.maisser@wunderbyte.at>
2023-11-12 11:00:54 +0100

    Revert "Add right namespaces"
    This reverts commit b6736bd3032937491c13d31c32b08599d31a6d36.

--- classes/external/search_teachers.php
+++ classes/external/search_teachers.php
@@ -15,9 +15,9 @@
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 namespace mod_booking\external;
-use core_external\external_api;
-use core_external\external_function_parameters;
-use core_external\external_value;
+use external_api;
+use external_function_parameters;
+use external_value;
 use mod_booking\booking;
 
 defined('MOODLE_INTERNAL') || die();
