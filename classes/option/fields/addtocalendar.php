2f6f868886a70852c360f98b19df24ec6dd033c7
Georg Maißer <georg.maisser@wunderbyte.at>
2023-12-19 21:20:51 +0100

    Add Addtocalendar logic

---
+++ classes/option/fields/addtocalendar.php
@@ -0,0 +1,100 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Control and manage booking dates.
+ *
+ * @package mod_booking
+ * @copyright 2023 Wunderbyte GmbH <info@wunderbyte.at>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_booking\option\fields;
+
+use mod_booking\option\fields_info;
+use mod_booking\option\field_base;
+use mod_booking\singleton_service;
+use MoodleQuickForm;
+use stdClass;
+
+/**
+ * Class to handle one property of the booking_option_settings class.
+ *
+ * @copyright Wunderbyte GmbH <info@wunderbyte.at>
+ * @author Georg Maißer
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class addtocalendar extends field_base {
+
+    /**
+     * This ID is used for sorting execution.
+     * @var int
+     */
+    public static $id = MOD_BOOKING_OPTION_FIELD_ADDTOCALENDAR;
+
+    /**
+     * Some fields are saved with the booking option...
+     * This is normal behaviour.
+     * Some can be saved only post save (when they need the option id).
+     * @var int
+     */
+    public static $save = MOD_BOOKING_EXECUTION_NORMAL;
+
+    /**
+     * This identifies the header under which this particular field should be displayed.
+     * @var string
+     */
+    public static $header = MOD_BOOKING_HEADER_DATES;
+
+    /**
+     * This function interprets the value from the form and, if useful...
+     * ... relays it to the new option class for saving or updating.
+     * @param stdClass $formdata
+     * @param stdClass $newoption
+     * @param mixed $returnvalue
+     * @return string // If no warning, empty string.
+     */
+    public static function prepare_save_field(
+        stdClass &$formdata,
+        stdClass &$newoption,
+        int $updateparam,
+        $returnvalue = 0): string {
+
+        return '';
+    }
+
+    /**
+     *
+     * @param MoodleQuickForm $mform
+     * @param array $formdata
+     * @param array $optionformconfig
+     * @return void
+     */
+    public static function instance_form_definition(MoodleQuickForm &$mform, array &$formdata, array $optionformconfig) {
+
+        // Add to course calendar dropdown.
+        $caleventtypes = [
+            0 => get_string('caldonotadd', 'mod_booking'),
+            1 => get_string('caladdascourseevent', 'mod_booking'),
+        ];
+        $mform->addElement('select', 'addtocalendar', get_string('addtocalendar', 'mod_booking'), $caleventtypes);
+
+        if (get_config('booking', 'addtocalendar_locked')) {
+            // If the setting is locked in settings.php it will be frozen.
+            $mform->freeze('addtocalendar');
+        }
+    }
+}
