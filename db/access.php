8aa0ca3affac017a28e225433a061215669f0752
Georg Maißer <georg.maisser@wunderbyte.at>
2024-01-05 10:32:27 +0100

    linting

--- db/access.php
+++ db/access.php
@@ -24,17 +24,17 @@
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = [
-    'mod/booking:view' => array(
+    'mod/booking:view' => [
         'captype' => 'read',
         'contextlevel' => CONTEXT_MODULE,
-        'archetypes' => array(
+        'archetypes' => [
             'guest' => CAP_ALLOW,
             'student' => CAP_ALLOW,
             'teacher' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
-            'manager' => CAP_ALLOW
-        )
-    ),
+            'manager' => CAP_ALLOW,
+        ],
+    ],
     'mod/booking:comment' => [
         'riskbitmask' => RISK_SPAM,
         'captype' => 'write',
