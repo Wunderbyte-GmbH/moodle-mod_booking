31c215523f0c5f0218e8e562216825450ccf1a1e
Andrii Semenets <semteacher@gmail.com>
2024-05-03 11:06:10 +0300

    fix inverting of logic condition in #491 (issue 4)

--- classes/bo_availability/bo_info.php
+++ classes/bo_availability/bo_info.php
@@ -569,8 +569,8 @@ class bo_info {
         // ... then we need to verify if we are already booked.
         // If not, we need to do it now.
 
-        if (!isset($conditions[$pagenumber]['pre']) 
-            && !$conditions[$pagenumber]['id'] === MOD_BOOKING_BO_COND_BOOKITBUTTON) {
+        if (!isset($conditions[$pagenumber]['pre'])
+            && !($conditions[$pagenumber]['id'] === MOD_BOOKING_BO_COND_BOOKITBUTTON)) {
 
             // Every time we load a page which is not "pre", we need to check if we are booked.
             // First, determine if this is a booking option with a price.
