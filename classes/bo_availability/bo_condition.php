33dd94f7b8441c7d4fca6fd75dea3a9cfb32eac1
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2024-05-07 09:49:33 +0200

    Codestyle

--- classes/bo_availability/bo_condition.php
+++ classes/bo_availability/bo_condition.php
@@ -153,7 +153,6 @@ interface bo_condition {
      * ... but actually hide the conditons alltogether.
      *
      * @param booking_option_settings $settings
-     * @param int $userid
      * @return array
      */
     public function return_sql(): array;
--- lib.php
+++ lib.php
@@ -266,7 +266,7 @@ define('MOD_BOOKING_FORM_COURSESTARTTIME', 'coursestarttime_');
 define('MOD_BOOKING_FORM_COURSEENDTIME', 'courseendtime_');
 define('MOD_BOOKING_FORM_DELETEDATE', 'deletedate_');
 
-//SQL Filter
+// SQL Filter.
 define('MOD_BOOKING_SQL_FILTER_INACTIVE', 0);
 define('MOD_BOOKING_SQL_FILTER_ACTIVE_JSON_BO', 1);
 define('MOD_BOOKING_SQL_FILTER_ACTIVE_BO_TIME', 2);
