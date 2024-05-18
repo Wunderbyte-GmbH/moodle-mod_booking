72f1c639125b123fe5cc538f9d71181b64eb0499
Andrii Semenets <semteacher@gmail.com>
2024-05-18 09:02:07 +0300

    draft of test_booking_bookit_with_price_and_cancellation() #454 (105 - CANCELMYSELF always in list of conditnos but 150 already booked has returned)

--- tests/bo_availability/condition_all_test.php
+++ tests/bo_availability/condition_all_test.php
@@ -64,6 +64,125 @@ class condition_all_test extends advanced_testcase {
     }
 
     /**
+     * Test of booking option with price as well as cancellation by user.
+     *
+     * @covers \condition\priceset::is_available
+     * @throws \coding_exception
+     * @throws \dml_exception
+     *
+     * @dataProvider booking_common_settings_provider
+     */
+    public function test_booking_bookit_with_price_and_cancellation() {
+        global $DB, $CFG;
+
+        // Set parems requred for cancellation.
+        $bdata['cancancelbook'] = 1;
+        if (class_exists('local_shopping_cart\shopping_cart')) {
+            set_config('cancelationfee', 1, 'local_shopping_cart');
+        }
+
+        // Setup test data.
+        $course1 = $this->getDataGenerator()->create_course(['enablecompletion' => 1]);
+        $course2 = $this->getDataGenerator()->create_course(['enablecompletion' => 1]);
+
+        // Create users.
+        $student1 = $this->getDataGenerator()->create_user();
+        $student2 = $this->getDataGenerator()->create_user();
+        $student3 = $this->getDataGenerator()->create_user();
+        $student4 = $this->getDataGenerator()->create_user();
+        $teacher = $this->getDataGenerator()->create_user();
+        $bookingmanager = $this->getDataGenerator()->create_user(); // Booking manager.
+
+        $bdata['course'] = $course1->id;
+        $bdata['bookingmanager'] = $bookingmanager->username;
+
+        $booking1 = $this->getDataGenerator()->create_module('booking', $bdata);
+
+        $this->setAdminUser();
+
+        $this->getDataGenerator()->enrol_user($student1->id, $course1->id);
+        $this->getDataGenerator()->enrol_user($student2->id, $course1->id);
+        $this->getDataGenerator()->enrol_user($student3->id, $course1->id);
+        $this->getDataGenerator()->enrol_user($student4->id, $course1->id);
+        $this->getDataGenerator()->enrol_user($teacher->id, $course1->id);
+        $this->getDataGenerator()->enrol_user($bookingmanager->id, $course1->id);
+
+        $record = new stdClass();
+        $record->bookingid = $booking1->id;
+        $record->text = 'Test option1';
+        $record->courseid = $course2->id;
+        $record->maxanswers = 2;
+        $record->useprice = 1; // Use price from the default category.
+
+        /** @var mod_booking_generator $plugingenerator */
+        $plugingenerator = self::getDataGenerator()->get_plugin_generator('mod_booking');
+
+        $pricecategorydata = (object)[
+            'ordernum' => 1,
+            'name' => 'default',
+            'identifier' => 'default',
+            'defaultvalue' => 100,
+            'pricecatsortorder' => 1,
+        ];
+
+        $plugingenerator->create_pricecategory($pricecategorydata);
+        $option1 = $plugingenerator->create_option($record);
+
+        $settings = singleton_service::get_instance_of_booking_option_settings($option1->id);
+        // To avoid retrieving the singleton with the wrong settings, we destroy it.
+        singleton_service::destroy_booking_singleton_by_cmid($settings->cmid);
+
+        // Book the first user without any problem.
+        $boinfo = new bo_info($settings);
+
+        // Check option availability if user is not logged yet.
+        require_logout();
+        list($id, $isavailable, $description) = $boinfo->is_available($settings->id, $student1->id, false);
+        $this->assertEquals(MOD_BOOKING_BO_COND_ISLOGGEDINPRICE, $id);
+
+        // Book option1 by the student1 himself.
+        $this->setUser($student1);
+
+        list($id, $isavailable, $description) = $boinfo->is_available($settings->id, $student1->id, true);
+        // The user sees now either the payment button or the noshoppingcart message.
+        if (class_exists('local_shopping_cart\shopping_cart')) {
+            $this->assertEquals(MOD_BOOKING_BO_COND_PRICEISSET, $id);
+        } else {
+            $this->assertEquals(MOD_BOOKING_BO_COND_NOSHOPPINGCART, $id);
+        }
+
+        $option = singleton_service::get_instance_of_booking_option($settings->cmid, $settings->id);
+
+        // Admin confirms the users booking.
+        $this->setAdminUser();
+        // Verify price.
+        $price = price::get_price('option', $settings->id);
+        // Default price expected.
+        $this->assertEquals($price["price"], 100);
+
+        // In this test, we book the user directly (we don't test the payment process).
+        $option->user_submit_response($student1, 0, 0, 0, MOD_BOOKING_VERIFIED);
+
+        // User 1 should be booked now.
+        list($id, $isavailable, $description) = $boinfo->is_available($settings->id, $student1->id, true);
+        $this->assertEquals(MOD_BOOKING_BO_COND_ALREADYBOOKED, $id);
+
+        // The student1 attempt to camcel purchase by himself.
+        $this->setUser($student1);
+
+        list($id, $isavailable, $description) = $boinfo->is_available($settings->id, $student1->id, true);
+        $this->assertEquals(MOD_BOOKING_BO_COND_ALREADYBOOKED, $id);
+        // Render to see if "cancel purchase" present.
+        $buttons = booking_bookit::render_bookit_button($settings, $student1->id);
+        $this->assertStringContainsString('Cancel purchase', $buttons);
+        // Actual cancellation of purcahse.
+        $result = booking_bookit::bookit('option', $settings->id, $student1->id);
+        list($id, $isavailable, $description) = $boinfo->is_available($settings->id, $student1->id, true);
+        $result = booking_bookit::bookit('option', $settings->id, $student1->id);
+        list($id, $isavailable, $description) = $boinfo->is_available($settings->id, $student1->id, true);
+    }
+
+    /**
      * Test booking, cancelation, option has started etc.
      *
      * @covers \condition\bookitbutton::is_available
