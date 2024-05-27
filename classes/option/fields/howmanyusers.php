f983bf16c4b624789e5d2b38c22dfafa7b3b8623
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2024-05-27 13:47:35 +0200

    GH-520 field howmanyusers

--- classes/option/fields/howmanyusers.php
+++ classes/option/fields/howmanyusers.php
@@ -26,6 +26,7 @@ namespace mod_booking\option\fields;
 
 use mod_booking\option\fields_info;
 use mod_booking\option\field_base;
+use mod_booking\singleton_service;
 use MoodleQuickForm;
 use stdClass;
 
@@ -91,7 +92,39 @@ class howmanyusers extends field_base {
         int $updateparam,
         $returnvalue = null): array {
 
-        return parent::prepare_save_field($formdata, $newoption, $updateparam, 0);
+        $changes = self::check_for_changes($formdata);
+
+        return $changes;
+    }
+
+    /**
+     * Return changes in array.
+     *
+     * @param stdClass $formdata
+     *
+     * @return array
+     *
+     */
+    private static function check_for_changes(stdClass $formdata): array {
+        $key = fields_info::get_class_name(static::class);
+        $value = $formdata->{$key} ?? '';
+
+        $changes = [];
+        // Check if there were changes and return these.
+        if (!empty($formdata->id) && isset($value)) {
+            $settings = singleton_service::get_instance_of_booking_option_settings($formdata->id);
+            $valueclass = new stdClass;
+            self::set_data($valueclass, $settings);
+            if ($valueclass->{$key} != $value) {
+                $changes = [
+                    'changes' => [
+                        'oldvalue' => $valueclass->{$key},
+                        'newvalue' => $value,
+                    ],
+                ];
+            }
+        }
+        return $changes;
     }
 
     /**
