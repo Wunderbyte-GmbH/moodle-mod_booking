0400436917fa2c4f29a05a4fcb86239d44942f0b
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2024-06-03 14:47:22 +0200

    Improvement: placeholder  booking_option_changes

---
+++ classes/placeholders/placeholders/booking_option_changes.php
@@ -0,0 +1,91 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Handle fields for booking option.
+ *
+ * @package mod_booking
+ * @copyright 2023 Wunderbyte GmbH <info@wunderbyte.at>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_booking\placeholders\placeholders;
+
+use mod_booking\placeholders\placeholders_info;
+use mod_booking\singleton_service;
+use moodle_exception;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/mod/booking/lib.php');
+
+/**
+ * Control and manage placeholders for booking instances, options and mails.
+ *
+ * @copyright Wunderbyte GmbH <info@wunderbyte.at>
+ * @author Georg Maißer
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class booking_option_changes {
+
+    /**
+     * Function which takes a text, replaces the placeholders...
+     * ... and returns the text with the correct values.
+     * @param int $cmid
+     * @param int $optionid
+     * @param int $userid
+     * @param int $installmentnr
+     * @param int $duedate
+     * @param float $price
+     * @param string $text
+     * @param array $params
+     * @param int $descriptionparam
+     * @param string $rulejson
+     * @return string
+     */
+    public static function return_value(
+        int $cmid = 0,
+        int $optionid = 0,
+        int $userid = 0,
+        int $installmentnr = 0,
+        int $duedate = 0,
+        float $price = 0,
+        string &$text = '',
+        array &$params = [],
+        int $descriptionparam = MOD_BOOKING_DESCRIPTION_WEBSITE,
+        string $rulejson = ''
+    ) {
+        $rulejson = json_decode($rulejson);
+        if (
+            !empty($rulejson) &&
+            isset($rulejson->datafromevent->other->changes[0]->info) &&
+            isset($rulejson->datafromevent->other->changes[0]->newvalue)
+        ) {
+            $value =
+                $rulejson->datafromevent->other->changes[0]->info .
+                $rulejson->datafromevent->other->changes[0]->newvalue;
+        } else {
+            throw new moodle_exception(
+                'paramnotpresent',
+                'mod_booking',
+                '',
+                '',
+                "Something went wrong with the changes.");
+        }
+
+        return $value;
+    }
+}
