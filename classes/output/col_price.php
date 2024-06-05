b2a08c8da62222229b2c6dd152f78cab9624d4af
Andrii Semenets <semteacher@gmail.com>
2024-06-05 21:41:41 +0300

    linting

--- classes/output/col_price.php
+++ classes/output/col_price.php
@@ -71,7 +71,12 @@ class col_price implements renderable, templatable {
      * @param context|null $context
      *
      */
-    public function __construct(stdClass $values, booking_option_settings $settings, ?object $buyforuser = null, ?context $context = null) {
+    public function __construct(
+        stdClass $values,
+        booking_option_settings $settings,
+        ?object $buyforuser = null,
+        ?context $context = null
+    ) {
 
         global $USER;
 
