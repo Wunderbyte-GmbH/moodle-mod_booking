6d75502302519339f80b1d0c184de2c45635e887
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2024-06-05 12:14:20 +0200

    Codestyle

--- classes/option/field_base.php
+++ classes/option/field_base.php
@@ -188,8 +188,8 @@ abstract class field_base implements fields {
     /**
      * Check if there is a difference between the former and the new values of the formdata.
      *
-     * @param object $formdata
-     * @param object $self
+     * @param stdClass $formdata
+     * @param stdClass $self
      * @param mixed $mockdata // Only needed if there the object needs params for the save_data function.
      * @param string $key
      * @param mixed $value
@@ -198,8 +198,8 @@ abstract class field_base implements fields {
      *
      */
     public function check_for_changes(
-        object $formdata,
-        object $self,
+        stdClass $formdata,
+        stdClass $self,
         mixed $mockdata = '',
         string $key = '',
         mixed $value = ''): array {
