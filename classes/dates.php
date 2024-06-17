cbbc8fac47b2078eb5b14d29377fb55de3e8ce9c
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2024-06-17 11:03:03 +0200

    Codestyle

--- classes/dates.php
+++ classes/dates.php
@@ -611,13 +611,11 @@ class dates {
 
         $od = [];
         $olddates = "";
-        if (empty($datestodelete) && empty($datestosave)) {
+        // Checking for changes.
+        if ((empty($datestodelete) && empty($datestosave))
+            || in_array('dates', MOD_BOOKING_CLASSES_EXCLUDED_FROM_CHANGES_TRACKING)) {
             return [];
         }
-        // Checking for changes.
-        if (in_array('dates', MOD_BOOKING_CLASSES_EXCLUDED_FROM_CHANGES_TRACKING)) {
-            return [];
-        };
 
         foreach ($memory as $old) {
             $olddate = dates_handler::prettify_datetime((int)$old->coursestarttime,
