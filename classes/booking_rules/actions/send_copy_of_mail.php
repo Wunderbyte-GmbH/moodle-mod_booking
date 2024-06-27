5e0fab26f4834853cd0730dcda127a6dd0faf144
Andrii Semenets <semteacher@gmail.com>
2024-06-27 18:30:34 +0300

    fix: String [from,core] is deprecated. / String [to,core] is deprecated.

--- classes/booking_rules/actions/send_copy_of_mail.php
+++ classes/booking_rules/actions/send_copy_of_mail.php
@@ -86,13 +86,13 @@ class send_copy_of_mail implements booking_rule_action {
 
         $userfrom = singleton_service::get_instance_of_user((int) $datafromevent['userid']);
         $userfromformatted = "$userfrom->firstname $userfrom->lastname &lt;$userfrom->email&gt;";
-        $userfromformatted = "<b>" . get_string('from') . "</b>: $userfromformatted<br>";
+        $userfromformatted = "<b>" . get_string('fromsender') . "</b>: $userfromformatted<br>";
 
         $usertoformatted = '';
         if (!empty($datafromevent['relateduserid'])) {
             $userto = singleton_service::get_instance_of_user((int) $datafromevent['relateduserid']);
             $usertoformatted .= "$userto->firstname $userto->lastname &lt;$userto->email&gt;";
-            $usertoformatted = "<b>" . get_string('to') . "</b>: $usertoformatted<br>";
+            $usertoformatted = "<b>" . get_string('torecipient') . "</b>: $usertoformatted<br>";
         }
 
         $this->subject = $actiondata->subjectprefix . ": " . $datafromevent['other']->subject;
