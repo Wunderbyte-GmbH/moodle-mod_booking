06c8fa8acb0f2121d1e37bb9f43bd0e3b807db68
Andrii Semenets <semteacher@gmail.com>
2024-07-19 22:40:44 +0300

    bump version to test strings installation

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024071903;
+$plugin->version = 2024071902;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->release = '8.5.0';
 $plugin->maturity = MATURITY_STABLE;
