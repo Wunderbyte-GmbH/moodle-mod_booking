4c19a5d1b6d6a5ca901e2f34a96db105f987c9d5
Georg Maißer <georg.maisser@wunderbyte.at>
2024-07-29 12:45:33 +0200

    Feature: Add places to booking_answers table

--- db/upgrade.php
+++ db/upgrade.php
@@ -3803,5 +3803,21 @@ function xmldb_booking_upgrade($oldversion) {
         // Booking savepoint reached.
         upgrade_mod_savepoint(true, 2024081600, 'booking');
     }
+
+    if ($oldversion < 2024072900) {
+
+        // Define field places to be added to booking_answers.
+        $table = new xmldb_table('booking_answers');
+        $field = new xmldb_field('places', XMLDB_TYPE_INTEGER, '10', null, null, null, '1', 'status');
+
+        // Conditionally launch add field places.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Booking savepoint reached.
+        upgrade_mod_savepoint(true, 2024072900, 'booking');
+    }
+
     return true;
 }
