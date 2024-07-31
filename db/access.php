79e43abaabbc56a1ea5a0641d099b9e73a00ac81
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2024-07-31 14:06:45 +0200

    Improvement: Allow coursecreators to update bookings

--- db/access.php
+++ db/access.php
@@ -105,6 +105,7 @@ $capabilities = [
         'contextlevel' => CONTEXT_MODULE,
         'archetypes' => [
             'teacher' => CAP_ALLOW,
+            'coursecreator' => CAP_ALLOW,
             'editingteacher' => CAP_ALLOW,
             'manager' => CAP_ALLOW,
         ],
