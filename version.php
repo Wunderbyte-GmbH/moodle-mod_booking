7db4bfef85b35c2cfe297d020b54d041e92296a6
Georg Maißer <georg.maisser@wunderbyte.at>
2024-09-05 10:44:01 +0200

    Version 8.6.14 (2024090500)

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024090400;
+$plugin->version = 2024090500;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
-$plugin->release = '8.6.13';
+$plugin->release = '8.6.14';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'mod_booking';
 $plugin->supported = [401, 404];
