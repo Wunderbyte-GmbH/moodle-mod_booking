01f95ab18f9862e76b53161b5e099486c22e3cf2
Fionarin <vonbouvierazula@gmail.com>
2024-09-17 11:41:25 +0200

    appearance settings non pro version

--- settings.php
+++ settings.php
@@ -240,6 +240,8 @@ if ($ADMIN->fulltree) {
         $settings->add(
             new admin_setting_heading('appearancesettings',
                 get_string('appearancesettings', 'mod_booking'),
+                get_string('prolicensefeatures', 'mod_booking') .
+                get_string('appearanceprofeatures', 'mod_booking') .
                 get_string('infotext:prolicensenecessary', 'mod_booking')));
     }
 
