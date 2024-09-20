370cff6c589646743b6268ba740865fce4fd544d
Georg Maißer <georg.maisser@wunderbyte.at>
2024-09-20 17:03:27 +0200

    Improvement: Open booking options template with returnurl -> go back to overview on save

--- classes/table/optiontemplatessettings_table.php
+++ classes/table/optiontemplatessettings_table.php
@@ -114,13 +114,16 @@ class optiontemplatessettings_table extends table_sql {
             ['optionid' => $values->optionid, 'action' => 'delete', 'id' => $this->cmid]);
         $output .= $OUTPUT->single_button($url, $delete, 'get');
         $edit = get_string('edit');
+        $returnurl = new moodle_url('/mod/booking/optiontemplatessettings.php', ['id' => $this->cmid]);
         $url = new moodle_url(
             '/mod/booking/editoptions.php',
             [
             'optionid' => $values->optionid,
             'id' => $this->cmid,
             'addastemplate' => '1',
-            ]);
+            'returnurl' => $returnurl->out(),
+            ]
+        );
         $output .= $OUTPUT->single_button($url, $edit, 'get');
         return $output;
     }
