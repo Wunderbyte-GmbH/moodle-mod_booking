51b69030a0a63e118f63c3c3619920cc83e98070
Andrii Semenets <semteacher@gmail.com>
2024-11-12 21:15:16 +0200

    new phpunit test_booking_campaign_blockbooking_customfields (#689 , incomplete)

--- tests/booking_campaigns/booking_campaigns_test.php
+++ tests/booking_campaigns/booking_campaigns_test.php
@@ -31,6 +31,7 @@ use coding_exception;
 use mod_booking_generator;
 use context_system;
 use mod_booking\bo_availability\bo_info;
+use mod_booking\booking_campaigns\campaigns_info;
 use stdClass;
 
 defined('MOODLE_INTERNAL') || die();
@@ -158,6 +159,192 @@ final class booking_campaigns_test extends advanced_testcase {
 
         // Book the first user without any problem.
         $boinfo1 = new bo_info($settings1);
+
+        $this->setUser($student1);
+        // Book option1 by student1.
+        $result = booking_bookit::bookit('option', $settings1->id, $student1->id);
+        $result = booking_bookit::bookit('option', $settings1->id, $student1->id);
+        list($id, $isavailable, $description) = $boinfo1->is_available($settings1->id, $student1->id, true);
+        $this->assertEquals(MOD_BOOKING_BO_COND_ALREADYBOOKED, $id);
+
+        // Try to book option1 with student2.
+        $this->setUser($student2);
+        list($id, $isavailable, $description) = $boinfo1->is_available($settings1->id, $student2->id, true);
+        $this->assertEquals(MOD_BOOKING_BO_COND_CAMPAIGN_BLOCKBOOKING, $id);
+
+        // Force campaing deletion.
+        $camps = campaigns_info::get_all_campaigns();
+        foreach ($camps as $camp) {
+            campaigns_info::delete_campaign($camp->id);
+        }
+        singleton_service::get_instance()->campaigns = [];
+    }
+
+    /**
+     * Test campaign blockbooking.
+     *
+     * @covers \condition\campaign_blockbooking::is_available
+     * @param array $bdata
+     * @throws \coding_exception
+     * @throws \dml_exception
+     *
+     * @dataProvider booking_campaigns_settings_provider
+     */
+    public function test_booking_campaign_blockbooking_customfields(array $bdata): void {
+        global $DB, $CFG;
+
+        // Create user profile custom fields.
+        $this->getDataGenerator()->create_custom_profile_field([
+            'datatype' => 'text', 'shortname' => 'ucustom1', 'name' => 'ucustom1',
+        ]);
+        $this->getDataGenerator()->create_custom_profile_field([
+            'datatype' => 'text', 'shortname' => 'ucustom2', 'name' => 'ucustom2',
+        ]);
+
+        $users = [
+            ['profile_field_ucustom1' => 'student', 'profile_field_ucustom2' => 'no'],
+            ['profile_field_ucustom1' => '', 'profile_field_ucustom2' => 'yes'],
+            ['profile_field_ucustom1' => 'teacher', 'profile_field_ucustom2' => ''],
+        ];
+
+        // Setup test data.
+        $course1 = $this->getDataGenerator()->create_course(['enablecompletion' => 1]);
+        $course2 = $this->getDataGenerator()->create_course(['enablecompletion' => 1]);
+
+        // Create users.
+        $student1 = $this->getDataGenerator()->create_user($users[0]);
+        $student2 = $this->getDataGenerator()->create_user($users[1]);
+        $teacher = $this->getDataGenerator()->create_user($users[2]);
+        $bookingmanager = $this->getDataGenerator()->create_user(); // Booking manager.
+
+        $bdata['course'] = $course1->id;
+        $bdata['bookingmanager'] = $bookingmanager->username;
+
+        $booking1 = $this->getDataGenerator()->create_module('booking', $bdata);
+
+        $this->setAdminUser();
+
+        $this->getDataGenerator()->enrol_user($student1->id, $course1->id, 'student');
+        $this->getDataGenerator()->enrol_user($student2->id, $course1->id, 'student');
+        $this->getDataGenerator()->enrol_user($teacher->id, $course1->id, 'editingteacher');
+        $this->getDataGenerator()->enrol_user($bookingmanager->id, $course1->id, 'editingteacher');
+
+        // Create booking custom field.
+        $categorydata            = new stdClass();
+        $categorydata->name      = 'BookCustomCat1';
+        $categorydata->component = 'mod_booking';
+        $categorydata->area      = 'booking';
+        $categorydata->itemid    = 0;
+        $categorydata->contextid = context_system::instance()->id;
+
+        $bookingcat = $this->getDataGenerator()->create_custom_field_category((array)$categorydata);
+        $bookingcat->save();
+
+        $fielddata                = new stdClass();
+        $fielddata->categoryid    = $bookingcat->get('id');
+        $fielddata->name       = 'bcustom1';
+        $fielddata->shortname  = 'bcustom1';
+        $fielddata->type = 'text';
+        $fielddata->configdata    = "{\"required\":\"0\",\"uniquevalues\":\"0\",\"locked\":\"0\",\"visibility\":\"2\",
+                                    \"defaultvalue\":\"\",\"displaysize\":30,\"maxlength\":50,\"ispassword\":\"0\",
+                                    \"link\":\"\",\"linktarget\":\"\"}";
+        $bookingfield = $this->getDataGenerator()->create_custom_field((array)$fielddata);
+        $bookingfield->save();
+        $this->assertTrue(\core_customfield\field::record_exists($bookingfield->get('id')));
+
+        /** @var mod_booking_generator $plugingenerator */
+        $plugingenerator = self::getDataGenerator()->get_plugin_generator('mod_booking');
+
+        $record = new stdClass();
+        $record->bookingid = $booking1->id;
+        $record->text = 'Test option1';
+        $record->chooseorcreatecourse = 1; // Reqiured.
+        $record->courseid = $course2->id;
+        $record->useprice = 0;
+        $record->bookedusers = 5;
+        $record->maxanswers = 6;
+        //$record->optiondateid_1 = "0";
+        //$record->daystonotify_1 = "0";
+        //$record->coursestarttime_1 = strtotime('now - 2 day');
+        //$record->courseendtime_1 = strtotime('now + 2 day');
+        $record->customfield_bcustom1 = 'exclude';
+
+        $option1 = $plugingenerator->create_option($record);
+        singleton_service::destroy_booking_option_singleton($option1->id); // Mandatory there.
+
+        $record = new stdClass();
+        $record->bookingid = $booking1->id;
+        $record->text = 'Test option1';
+        $record->chooseorcreatecourse = 1; // Reqiured.
+        $record->courseid = $course2->id;
+        $record->useprice = 0;
+        $record->bookedusers = 0;
+        $record->maxanswers = 6;
+        //$record->optiondateid_1 = "0";
+        //$record->daystonotify_1 = "0";
+        //$record->coursestarttime_1 = strtotime('now - 2 day');
+        //$record->courseendtime_1 = strtotime('now + 2 day');
+        $record->customfield_bcustom1 = '';
+
+        $option2 = $plugingenerator->create_option($record);
+        singleton_service::destroy_booking_option_singleton($option2->id); // Mandatory there.
+
+        $record = new stdClass();
+        $record->bookingid = $booking1->id;
+        $record->text = 'Test option1';
+        $record->chooseorcreatecourse = 1; // Reqiured.
+        $record->courseid = $course2->id;
+        $record->useprice = 0;
+        $record->bookedusers = 2;
+        $record->maxanswers = 6;
+        //$record->optiondateid_1 = "0";
+        //$record->daystonotify_1 = "0";
+        //$record->coursestarttime_1 = strtotime('now - 2 day');
+        //$record->courseendtime_1 = strtotime('now + 2 day');
+        $record->customfield_bcustom1 = 'include';
+
+        $option3 = $plugingenerator->create_option($record);
+        singleton_service::destroy_booking_option_singleton($option3->id); // Mandatory there.
+
+        // Create blocking campaing.
+        $campaingdata = (object)[
+            'bofieldname' => 'bcustom1',
+            'fieldvalue' => 'exclude',
+            'campaignfieldnameoperator' => '!~', // Does not contain!
+            'cpfield' => 'ucustom1',
+            'cpoperator' => '~',
+            'cpvalue' => 'student',
+            'blockoperator' => 'blockbelow',
+            'blockinglabel' => 'block_below_50',
+            'hascapability' => null,
+            'percentageavailableplaces' => 50,
+        ];
+        $campaing = new stdClass();
+        $campaing = [
+            'name' => 'bloking1', 'type' => 1,
+            'starttime' => strtotime('yesterday'), 'endtime' => strtotime('now + 1 day'),
+            'pricefactor' => 1, 'limitfactor' => 1,
+            'json' => json_encode($campaingdata),
+        ];
+
+        $plugingenerator->create_campaign($campaing);
+        $camps = campaigns_info::get_all_campaigns();
+
+        $settings1 = singleton_service::get_instance_of_booking_option_settings($option1->id);
+        $optionobj1 = singleton_service::get_instance_of_booking_option($settings1->cmid, $option1->id);
+        $boinfo1 = new bo_info($settings1);
+        $settings2 = singleton_service::get_instance_of_booking_option_settings($option2->id);
+        $optionobj2 = singleton_service::get_instance_of_booking_option($settings2->cmid, $option2->id);
+        $boinfo2 = new bo_info($settings2);
+        $settings3 = singleton_service::get_instance_of_booking_option_settings($option3->id);
+        $optionobj3 = singleton_service::get_instance_of_booking_option($settings3->cmid, $option3->id);
+        $boinfo3 = new bo_info($settings3);
+        list($id, $isavailable, $description) = $boinfo1->is_available($settings1->id, $student1->id, true);
+        list($id, $isavailable, $description) = $boinfo1->is_available($settings2->id, $student1->id, true);
+        list($id, $isavailable, $description) = $boinfo1->is_available($settings3->id, $student1->id, true);
+        list($id, $isavailable, $description) = $boinfo1->is_available($settings1->id, $student2->id, true);
+        list($id, $isavailable, $description) = $boinfo1->is_available($settings2->id, $student2->id, true);
+        list($id, $isavailable, $description) = $boinfo1->is_available($settings3->id, $student2->id, true);
 
         $this->setUser($student1);
         // Book option1 by student1.
