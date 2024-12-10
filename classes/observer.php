ed788b0cabb229d074c04911f25f4d83e84c5177
Bernhard Fischer <bernhard.johannes.fischer@gmail.com>
2024-12-10 13:32:28 +0100

    Bugfix: Correctly create and delete user events (for each session) for teachers of booking options.

--- classes/observer.php
+++ classes/observer.php
@@ -252,6 +252,7 @@ class mod_booking_observer {
     public static function bookingoptiondate_created(\mod_booking\event\bookingoptiondate_created $event) {
 
         $optionid = $event->other['optionid'];
+        $settings = singleton_service::get_instance_of_booking_option_settings($optionid);
 
         if (empty($optionid)) {
             return;
@@ -266,6 +267,11 @@ class mod_booking_observer {
         $users = $bookingoption->get_all_users_booked();
         foreach ($users as $user) {
             new calendar($event->contextinstanceid, $optionid, $user->userid,
+                calendar::MOD_BOOKING_TYPEOPTIONDATE, $event->objectid, 1);
+        }
+        // Also create calendar events for teachers.
+        foreach ($settings->teacherids as $teacherid) {
+            new calendar($event->contextinstanceid, $optionid, $teacherid,
                 calendar::MOD_BOOKING_TYPEOPTIONDATE, $event->objectid, 1);
         }
     }
@@ -328,6 +334,7 @@ class mod_booking_observer {
             // phpcs:ignore Squiz.PHP.CommentedOutCode.Found
             /* new calendar($tmpcmid->id, $value->id, 0, calendar::MOD_BOOKING_TYPEOPTION); */
 
+            // phpcs:ignore moodle.Commenting.TodoComment.MissingInfoInline
             // TODO: We have to re-write this function so all calendar entries of optiondates will get updated correctly.
 
             $allteachers = $DB->get_records_sql("SELECT userid FROM {booking_teachers} WHERE optionid = ? AND calendarid > 0",
@@ -345,7 +352,18 @@ class mod_booking_observer {
      * @param \mod_booking\event\teacher_added $event
      */
     public static function teacher_added(\mod_booking\event\teacher_added $event) {
-        new calendar($event->contextinstanceid, $event->objectid, $event->relateduserid, calendar::MOD_BOOKING_TYPETEACHERADD);
+        $optionid = $event->objectid;
+        if (empty($optionid)) {
+            return;
+        }
+        $teacherid = $event->relateduserid;
+        $cmid = $event->contextinstanceid;
+        $settings = singleton_service::get_instance_of_booking_option_settings($optionid);
+        // Create calendar events for the newly added teacher.
+        foreach ($settings->sessions as $session) {
+            new calendar($cmid, $optionid, $teacherid,
+                calendar::MOD_BOOKING_TYPEOPTIONDATE, $session->id, 1);
+        }
     }
 
     /**
