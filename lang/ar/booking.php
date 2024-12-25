7f8a899f688fab55ff705a658c7544da2f00d254
Wail Abualela <19847088+wabualela@users.noreply.github.com>
2024-12-25 10:27:39 +0300

    translating

---
+++ lang/ar/booking.php
@@ -0,0 +1,227 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * English lang strings of the booking module
+ *
+ * @package mod_booking
+ * @copyright 2023 Wunderbyte GmbH <info@wunderbyte.at>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['aboutmodaloptiondateform'] = 'إنشاء تواريخ مخصصة
+(مثل الأحداث المحجوزة أو للتواريخ الفردية التي تختلف عن سلسلة التواريخ).';
+$string['accept'] = 'قبول';
+$string['accessdenied'] = 'تم رفض الوصول';
+$string['actionoperator'] = 'الإجراء';
+$string['actionoperator:adddate'] = 'إضافة تاريخ';
+$string['actionoperator:set'] = 'استبدال';
+$string['actionoperator:subtract'] = 'طرح';
+$string['actions'] = 'إجراءات الحجز';
+$string['activatemails'] = 'تفعيل البريد الإلكتروني (التأكيدات، الإشعارات والمزيد)';
+$string['active'] = "نشط";
+$string['activebookingoptions'] = 'خيارات الحجز النشطة';
+$string['activitycompletionsuccess'] = 'تم وضع علامة على جميع المستخدمين المحددين لإكمال النشاط';
+$string['activitycompletiontext'] = 'الرسالة التي سيتم إرسالها إلى المستخدم عند اكتمال خيار الحجز';
+$string['activitycompletiontextmessage'] = 'لقد أكملت خيار الحجز التالي:
+{$a->bookingdetails}
+اذهب إلى الدورة: {$a->courselink}
+شاهد جميع خيارات الحجز: {$a->bookinglink}';
+$string['activitycompletiontextsubject'] = 'اكتمل خيار الحجز';
+$string['addastemplate'] = 'إضافة كقالب';
+$string['addbookingcampaign'] = 'إضافة حملة';
+$string['addbookingrule'] = 'إضافة قاعدة';
+$string['addcategory'] = 'تحرير الفئات';
+$string['addcomment'] = 'إضافة تعليق...';
+$string['addcustomfieldorcomment'] = 'إضافة تعليق أو حقل مخصص';
+$string['adddatebutton'] = "إضافة تاريخ";
+$string['addedrecords'] = '{$a} سجل(ات) مضافة.';
+$string['addholiday'] = 'إضافة عطلة(ات)';
+$string['additionalpricecategories'] = 'إضافة أو تحرير فئات الأسعار';
+$string['addmorebookings'] = 'إضافة المزيد من الحجوزات';
+$string['addnewcategory'] = 'إضافة فئة جديدة';
+$string['addnewreporttemplate'] = 'إضافة قالب تقرير جديد';
+$string['addnewtagtemplate'] = 'إضافة قالب علامة جديد';
+$string['addoptiondate'] = 'إضافة تاريخ';
+$string['addoptiondateseries'] = 'إنشاء سلسلة تواريخ';
+$string['addpricecategory'] = 'إضافة فئة سعر';
+$string['addpricecategoryinfo'] = 'يمكنك إضافة فئة سعر أخرى';
+$string['address'] = 'العنوان';
+$string['addsemester'] = 'إضافة فصل دراسي';
+$string['addtocalendar'] = 'إضافة إلى تقويم الدورة';
+$string['addtocalendardesc'] = 'أحداث تقويم الدورة مرئية لجميع المستخدمين داخل الدورة. إذا كنت لا تريد إنشاءها على الإطلاق،
+يمكنك إيقاف تشغيل هذا الإعداد وقفله بشكل افتراضي. لا تقلق: سيتم إنشاء أحداث تقويم المستخدم لخيار الحجز المحجوز على أي حال.';
+$string['addtogroup'] = 'تسجيل المستخدمين تلقائيًا في المجموعة';
+$string['addtogroup_help'] = 'تسجيل المستخدمين تلقائيًا في المجموعة - سيتم إنشاء المجموعة تلقائيًا بالاسم: اسم الحجز - اسم الخيار';
+$string['addusertogroup'] = 'إضافة المستخدم إلى المجموعة: ';
+$string['adminparameter_desc'] = "استخدام المعلمات التي تم تعيينها في إعدادات المسؤول.";
+$string['adminparametervalue'] = "معلمة المسؤول";
+$string['advancedoptions'] = 'خيارات متقدمة';
+$string['aftercompletedtext'] = 'بعد اكتمال النشاط';
+$string['aftercompletedtext_help'] = 'الرسالة التي تظهر بعد اكتمال النشاط';
+$string['aftersubmitaction'] = 'بعد الحفظ...';
+$string['age'] = 'العمر';
+$string['alertrecalculate'] = '<b>تحذير!</b> سيتم إعادة حساب جميع الأسعار وسيتم استبدال جميع الأسعار القديمة.';
+$string['allbookingoptions'] = 'تنزيل المستخدمين لجميع خيارات الحجز';
+$string['allchangessaved'] = 'تم حفظ جميع التغييرات.';
+$string['allcohortsmustbefound'] = 'يجب أن يكون المستخدم عضوًا في جميع المجموعات';
+$string['allcomments'] = 'يمكن للجميع التعليق';
+$string['allcoursesmustbefound'] = 'يجب أن يكون المستخدم مشتركًا في جميع الدورات';
+$string['allmailssend'] = 'تم إرسال جميع رسائل البريد الإلكتروني إلى المستخدمين!';
+$string['allmoodleusers'] = 'جميع مستخدمي هذا الموقع';
+$string['alloptionsinreport'] = 'تقرير واحد لنشاط الحجز ' . '<span class="badge bg-success text-light"><i class="fa fa-cogs" aria-hidden="true"></i> PRO</span>';
+$string['alloptionsinreportdesc'] = 'سيشمل تقرير خيار الحجز الواحد جميع الحجوزات لجميع خيارات الحجز داخل هذه الحالة.';
+$string['allowbookingafterstart'] = 'السماح بالحجز بعد بدء الدورة';
+$string['allowoverbooking'] = 'السماح بالحجز الزائد';
+$string['allowoverbookingheader'] = 'الحجز الزائد لخيارات الحجز ' . '<span class="badge bg-success text-light"><i class="fa fa-cogs" aria-hidden="true"></i> PRO</span>';
+$string['allowoverbookingheader_desc'] = 'السماح للمسؤولين والمستخدمين المخولين بالحجز الزائد لخيارات الحجز.
+    (كن حذرًا: قد يؤدي ذلك إلى سلوك غير متوقع. قم بتفعيل هذا فقط إذا كنت بحاجة إليه حقًا.)';
+$string['allowupdate'] = 'السماح بتحديث الحجز';
+$string['allowupdatedays'] = 'الأيام قبل التاريخ المرجعي';
+$string['allratings'] = 'يمكن للجميع التقييم';
+$string['allteachers'] = 'جميع المعلمين';
+$string['allusersbooked'] = 'تم تعيين جميع المستخدمين المحددين {$a} بنجاح لهذا الخيار.';
+$string['alreadyonlist'] = 'سيتم إخطارك';
+$string['alreadypassed'] = 'تم اجتيازه بالفعل';
+$string['always'] = 'دائما';
+$string['andotherfield'] = "و حقل إضافي";
+$string['annotation'] = 'تعليق داخلي';
+$string['answer'] = "إجابة";
+$string['answered'] = 'تمت الإجابة';
+$string['appearancesettings'] = 'المظهر ' . '<span class="badge bg-success text-light"><i class="fa fa-cogs" aria-hidden="true"></i> PRO</span>';
+$string['appearancesettings_desc'] = 'تكوين مظهر مكون الحجز.';
+$string['apply'] = 'تطبيق';
+$string['applyunitfactor'] = 'تطبيق عامل الوحدة';
+$string['applyunitfactor_desc'] = 'إذا كان هذا الإعداد نشطًا، سيتم استخدام طول الوحدة التعليمية (مثل 45 دقيقة) المحدد أعلاه
+لحساب عدد الوحدات التعليمية. سيتم استخدام هذا العدد كعامل لصيغة السعر.
+مثال: يحتوي خيار الحجز على سلسلة تواريخ مثل "الإثنين، 15:00 - 16:30". لذا يستمر لمدة وحدتين تعليميتين (45 دقيقة لكل منهما).
+لذا سيتم تطبيق عامل وحدة 2 على صيغة السعر. (سيتم تطبيق عامل الوحدة فقط إذا كانت صيغة السعر موجودة.)';
+$string['areyousure:book'] = 'انقر مرة أخرى لتأكيد الحجز';
+$string['areyousure:cancel'] = 'انقر مرة أخرى لتأكيد الإلغاء';
+$string['asglobaltemplate'] = 'استخدام كقالب عالمي';
+$string['assesstimefinish'] = 'نهاية فترة التقييم';
+$string['assesstimestart'] = 'بداية فترة التقييم';
+$string['assignteachers'] = 'تعيين المعلمين:';
+$string['associatedcourse'] = 'الدورة المرتبطة';
+$string['astemplate'] = 'استخدام كقالب في هذه الدورة';
+$string['attachedfiles'] = 'الملفات المرفقة';
+$string['attachicalfile'] = 'إرفاق ملف iCal';
+$string['attachicalfile_desc'] = 'إرفاق ملفات iCal التي تحتوي على تاريخ (تواريخ) خيار الحجز إلى رسائل البريد الإلكتروني.';
+$string['attachment'] = 'المرفقات';
+$string['autcrheader'] = '[مهمل] إنشاء خيار الحجز التلقائي';
+$string['autcrwhatitis'] = 'إذا تم تمكين هذا الخيار، فإنه ينشئ تلقائيًا خيار حجز جديد ويعين مستخدمًا كمدير حجز / معلم له. يتم اختيار المستخدمين بناءً على قيمة حقل ملف تعريف المستخدم المخصص.';
+$string['autoenrol'] = 'تسجيل المستخدمين تلقائيًا';
+$string['autoenrol_help'] = 'إذا تم التحديد، سيتم تسجيل المستخدمين في الدورة ذات الصلة بمجرد إجراء الحجز وإلغاء تسجيلهم من تلك الدورة بمجرد إلغاء الحجز.';
+$string['automaticcoursecreation'] = 'إنشاء دورات Moodle تلقائيًا ' . '<span class="badge bg-success text-light"><i class="fa fa-cogs" aria-hidden="true"></i> PRO</span>';
+$string['availability'] = 'التوافر';
+$string['availabilityconditions'] = 'شروط التوافر';
+$string['availabilityconditionsheader'] = '<i class="fa fa-fw fa-key" aria-hidden="true"></i>&nbsp;شروط التوافر';
+$string['availabilityinfotextsheading'] = 'نصوص معلومات التوافر لأماكن الحجز وقائمة الانتظار ' . '<span class="badge bg-success text-light"><i class="fa fa-cogs" aria-hidden="true"></i> PRO</span>';
+$string['available'] = 'الأماكن المتاحة';
+$string['availableplaces'] = 'الأماكن المتاحة: {$a->available} من {$a->maxanswers}';
+$string['availplacesfull'] = 'ممتلئ';
+$string['backtoresponses'] = '&lt;&lt; العودة إلى الردود';
+$string['badge:exp'] = '<span class="badge bg-danger text-light"><i class="fa fa-flask" aria-hidden="true"></i> تجريبي</span>';
+$string['badge:pro'] = '<span class="badge bg-success text-light"><i class="fa fa-cogs" aria-hidden="true"></i> PRO</span>';
+$string['banusernames'] = 'حظر أسماء المستخدمين';
+$string['banusernames_help'] = 'لتحديد أسماء المستخدمين التي لا يمكنها التقديم، اكتب في هذا الحقل وافصل بينها بفاصلة. لحظر أسماء المستخدمين التي تنتهي بـ gmail.com و yahoo.com، اكتب: gmail.com, yahoo.com';
+$string['banusernameswarning'] = "اسم المستخدم الخاص بك محظور لذلك لا يمكنك الحجز.";
+$string['beforebookedtext'] = 'قبل الحجز';
+$string['beforebookedtext_help'] = 'الرسالة التي تظهر قبل حجز الخيار';
+$string['beforecompletedtext'] = 'بعد الحجز';
+$string['beforecompletedtext_help'] = 'الرسالة التي تظهر بعد حجز الخيار';
+$string['bigbluebuttonmeeting'] = 'اجتماع BigBlueButton';
+$string['biggerthan'] = 'أكبر من (رقم)';
+$string['billboardtext'] = 'النص الذي سيتم عرضه بدلاً من نص التنبيه المحظور';
+$string['blockabove'] = 'حظر أعلاه';
+$string['blockalways'] = 'الحظر بغض النظر عن الأماكن';
+$string['blockbelow'] = 'حظر أدناه';
+$string['blockinglabel'] = 'رسالة عند الحظر';
+$string['blockinglabel_help'] = 'أدخل الرسالة التي يجب أن تظهر عند حظر الحجز.
+إذا كنت تريد ترجمة هذه الرسالة، يمكنك استخدام
+<a href="https://docs.moodle.org/403/en/Multi-language_content_filter" target="_blank">مرشحات اللغة</a>.';
+$string['blockoperator'] = 'المشغل';
+$string['blockoperator_help'] = '<b>حظر أعلاه</b> ... سيتم حظر الحجز عبر الإنترنت بمجرد الوصول إلى النسبة المئوية المحددة
+من الحجوزات. سيكون الحجز ممكنًا فقط لأمين الصندوق أو المسؤول بعد ذلك.<br>
+<b>حظر أدناه</b> ... سيتم حظر الحجز عبر الإنترنت حتى الوصول إلى النسبة المئوية المحددة
+من الحجوزات. قبل حدوث ذلك، يكون الحجز ممكنًا فقط لأمين الصندوق أو المسؤول.';
+$string['boactioncancelbookingdesc'] = "يستخدم للخيارات التي يمكن شراؤها عدة مرات";
+$string['boactioncancelbookingvalue'] = "تفعيل الإلغاء الفوري";
+$string['boactionname'] = "اسم الإجراء";
+$string['boactions'] = 'الإجراءات بعد الحجز ' . '<span class="badge bg-success text-light"><i class="fa fa-cogs" aria-hidden="true"></i> PRO</span>' . ' ' . '<span class="badge bg-danger text-light"><i class="fa fa-flask" aria-hidden="true"></i> تجريبي</span>';
+$string['boactions_desc'] = "الإجراءات بعد الحجز لا تزال ميزة تجريبية. يمكنك تجربتها إذا أردت.
+لكن لا تستخدمها في بيئة إنتاجية حتى الآن!";
+$string['boactionselectuserprofilefield'] = "اختر حقل ملف التعريف";
+$string['boactionuserprofilefieldvalue'] = 'القيمة';
+$string['bocondallowedtobookininstance'] = "الحجز بدون القدرة";
+$string['bocondallowedtobookininstanceanyways'] = "السماح بالحجز بدون القدرة 'mod/booking:choose'";
+$string['bocondallowedtobookininstanceavailable'] = 'احجزه';
+$string['bocondallowedtobookininstancefullavailable'] = 'الحجز ممكن';
+$string['bocondallowedtobookininstancefullnotavailable'] = 'لا يوجد حق في الحجز في هذه الحالة';
+$string['bocondallowedtobookininstancenotavailable'] = 'لا يوجد حق في الحجز';
+$string['bocondalreadybooked'] = 'محجوز بالفعل: تم حجزه بالفعل من قبل هذا المستخدم';
+$string['bocondalreadybookedavailable'] = 'لم يتم حجزه بعد';
+$string['bocondalreadybookedfullavailable'] = 'لم يتم حجزه بعد من قبل المستخدم';
+$string['bocondalreadybookedfullnotavailable'] = 'محجوز';
+$string['bocondalreadybookednotavailable'] = 'محجوز';
+$string['bocondalreadyreserved'] = 'محجوز بالفعل: تمت إضافته بالفعل إلى السلة من قبل هذا المستخدم';
+$string['bocondalreadyreservedavailable'] = 'لم تتم إضافته بعد إلى السلة';
+$string['bocondalreadyreservedfullavailable'] = 'لم تتم إضافته بعد إلى السلة';
+$string['bocondalreadyreservedfullnotavailable'] = 'مضاف إلى السلة';
+$string['bocondalreadyreservednotavailable'] = 'مضاف إلى السلة';
+$string['bocondaskforconfirmation'] = 'طلب التأكيد: تأكيد الحجز يدويًا';
+$string['bocondaskforconfirmationavailable'] = 'احجزه';
+$string['bocondaskforconfirmationfullavailable'] = 'الحجز ممكن';
+$string['bocondaskforconfirmationfullnotavailable'] = 'احجزه - في قائمة الانتظار';
+$string['bocondaskforconfirmationnotavailable'] = 'احجزه - في قائمة الانتظار';
+$string['bocondbookingclosingtimefullnotavailable'] = 'لا يمكن حجزه بعد الآن (انتهى في {$a}).';
+$string['bocondbookingclosingtimenotavailable'] = 'لا يمكن حجزه بعد الآن (انتهى في {$a}).';
+$string['bocondbookingopeningtimefullnotavailable'] = 'يمكن حجزه من {$a}.';
+$string['bocondbookingopeningtimenotavailable'] = 'يمكن حجزه من {$a}.';
+$string['bocondbookingpolicy'] = 'سياسة الحجز';
+$string['bocondbookingtime'] = 'قابل للحجز فقط ضمن وقت معين';
+$string['bocondbookingtimeavailable'] = 'ضمن أوقات الحجز العادية.';
+$string['bocondbookingtimenotavailable'] = 'ليس ضمن أوقات الحجز العادية.';
+$string['bocondbookitbutton'] = 'زر الحجز: عرض زر الحجز العادي.';
+$string['bocondcapbookingchooseavailable'] = 'حق الحجز';
+$string['bocondcapbookingchoosefullavailable'] = 'حق الحجز في هذه الحالة';
+$string['bocondcapbookingchoosefullnotavailable'] = 'لا يوجد حق في الحجز في هذه الحالة';
+$string['bocondcapbookingchoosenotavailable'] = 'لا يوجد حق في الحجز';
+$string['bocondcustomform'] = 'املأ النموذج';
+$string['bocondcustomformavailable'] = 'احجزه';
+$string['bocondcustomformdeleteinfoscheckboxuser'] = 'مربع اختيار لحذف الإجابات';
+$string['bocondcustomformdeleteinfoscheckboxusertext'] = 'هل ترغب في حذف المعلومات المقدمة هنا بعد انتهاء الحدث؟';
+$string['bocondcustomformfullavailable'] = 'الحجز ممكن';
+$string['bocondcustomformfullnotavailable'] = 'الحجز غير ممكن';
+$string['bocondcustomformfullybooked'] = 'الخيار "{$a}" محجوز بالكامل بالفعل.';
+$string['bocondcustomformlabel'] = "التسمية";
+$string['bocondcustomformmail'] = "البريد الإلكتروني";
+$string['bocondcustomformmailerror'] = "عنوان البريد الإلكتروني غير صالح.";
+$string['bocondcustomformnotavailable'] = 'احجزه';
+$string['bocondcustomformnotempty'] = 'يجب ألا يكون فارغًا';
+$string['bocondcustomformnumberserror'] = "يرجى إدخال عدد صحيح من الأيام.";
+$string['bocondcustomformrestrict'] = 'يجب ملء النموذج قبل الحجز';
+$string['bocondcustomformstillavailable'] = "لا يزال متاحًا";
+$string['bocondcustomformurl'] = "الرابط";
+$string['bocondcustomformurlerror'] = "الرابط غير صالح أو لا يبدأ بـ http أو https.";
+$string['bocondcustomformvalue'] = 'القيمة';
+$string['bocondcustomformvalue_help'] = 'عند تحديد قائمة منسدلة، يرجى إدخال قيمة واحدة لكل سطر. يمكن إدخال القيم والقيم المعروضة بشكل منفصل، على سبيل المثال، "1 => قيمتي الأولى => number_of_availability" إلخ.';
+$string['bocondcustomuserprofilefieldavailable'] = 'احجزه';
+$string['bocondcustomuserprofilefieldconnectsecondfield'] = 'الاتصال بحقل ملف تعريف آخر';
+$string['bocondcustomuserprofilefieldfield'] = 'حقل ملف التعريف';
+$string['bocondcustomuserprofilefieldfield2'] = 'حقل ملف التعريف الثاني';
+$string['bocondcustomuserprofilefieldfullavailable'] = 'الحجز ممكن';
+$string['bocondcustomuserprofilefieldfullnotavailable'] = 'يسمح بالحجز فقط للمستخدمين الذين تم تعيين حقل ملف التعريف المخصص {$a->profilefield} لهم إلى القيمة {$a->value}.
+    <br>ولكن لديك الحق في حجز مستخدم على أي حال.';
