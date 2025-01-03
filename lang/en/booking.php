86381a6f1b2287751b0f2143832078a15ef280b7
Andrii Semenets <semteacher@gmail.com>
2025-01-03 18:04:22 +0200

    linting

--- lang/en/booking.php
+++ lang/en/booking.php
@@ -592,6 +592,7 @@ $string['cachedef_cachedpricecategories'] = 'Booking price categories (cache)';
 $string['cachedef_cachedprices'] = 'Prices in booking (cache)';
 $string['cachedef_cachedsemesters'] = 'Semesters (cache)';
 $string['cachedef_cachedteachersjournal'] = 'Teaches journal (Cache)';
+$string['cachedef_cacheduserprices'] = 'Individual prices in booking (cache)';
 $string['cachedef_conditionforms'] = 'Condition Forms (Cache)';
 $string['cachedef_confirmbooking'] = 'Booking confirmed (Cache)';
 $string['cachedef_customformuserdata'] = 'Custom form user data (Cache)';
