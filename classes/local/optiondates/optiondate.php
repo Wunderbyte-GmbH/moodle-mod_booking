8f729e3d175c872055240e5608d3d53dda8fa50d
Georg Maißer <georg.maisser@wunderbyte.at>
2025-01-27 12:28:10 +0100

    Improvement: Add optiondates class to handle presence status & attendance #721

---
+++ classes/local/optiondates/optiondate.php
@@ -0,0 +1,170 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The cartstore class handles the in and out of the cache.
+ *
+ * @package mod_booking
+ * @author Georg Maißer
+ * @copyright 2025 Wunderbyte GmbH
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_booking\local\optiondates;
+
+use stdClass;
+
+
+/**
+ * Class to handle optiondates, presence status etc.
+ */
+class optiondate {
+    /**
+     * $userid
+     *
+     * @var int
+     */
+    private $userid;
+    /**
+     * $optiondateid]
+     *
+     * @var int
+     */
+    private $optiondateid;
+
+    /**
+     * $optionid]
+     *
+     * @var int
+     */
+    private $optionid;
+
+    /**
+     * Constructor to initialize the class with userid, optiondateid, and optionid.
+     *
+     * @param int $userid
+     * @param int $optiondateid
+     * @param int $optionid
+     */
+    public function __construct($userid, $optiondateid, $optionid) {
+        $this->userid = $userid;
+        $this->optiondateid = $optiondateid;
+        $this->optionid = $optionid;
+    }
+
+
+    /**
+     * Create or update a record in the booking_optiondates_answers table.
+     *
+     * @param int|null $status
+     * @param string|null $notes
+     * @param string|null $json
+     * @return bool True on success, false on failure.
+     */
+    public function save_record($status = null, $notes = null, $json = null) {
+        global $DB, $USER;
+
+        $data = [
+            'userid' => $this->userid,
+            'optiondateid' => $this->optiondateid,
+            'optionid' => $this->optionid,
+            'status' => $status,
+            'notes' => $notes,
+            'timemodified' => time(),
+            'timecreated' => time(),
+            'usermodified' => $USER->id,
+            'json' => $json,
+        ];
+
+        if (
+            $existing = $DB->get_record(
+                'booking_optiondates_answers',
+                ['userid' => $this->userid, 'optiondateid' => $this->optiondateid, 'optionid' => $this->optionid]
+            )
+        ) {
+            $data['id'] = $existing->id;
+            $data['notes'] = $notes ?? $existing->notes ?? null;
+            $data['status'] = $status ?? $existing->status ?? 0;
+            $data['timecreated'] = $existing->timecreated;
+            return $DB->update_record('booking_optiondates_answers', $data);
+        } else {
+            return $DB->insert_record('booking_optiondates_answers', $data);
+        }
+    }
+
+    /**
+     * Retrieve a record from the booking_optiondates_answers table.
+     *
+     * @return stdClass|null The record object or null if not found.
+     */
+    public function get_record() {
+        global $DB;
+        return $DB->get_record(
+            'booking_optiondates_answers',
+            ['userid' => $this->userid, 'optiondateid' => $this->optiondateid, 'optionid' => $this->optionid]
+        );
+    }
+
+    /**
+     * Delete a record from the booking_optiondates_answers table.
+     *
+     * @return bool True on success, false on failure.
+     */
+    public function delete_record() {
+        global $DB;
+        return $DB->delete_records(
+            'booking_optiondates_answers',
+            ['userid' => $this->userid, 'optiondateid' => $this->optiondateid, 'optionid' => $this->optionid]
+        );
+    }
+
+    /**
+     * Retrieve all records for a specific optiondate.
+     *
+     * @return array An array of record objects.
+     */
+    public function get_records_by_optiondate() {
+        global $DB;
+        return $DB->get_records(
+            'booking_optiondates_answers',
+            ['optiondateid' => $this->optiondateid, 'optionid' => $this->optionid]
+        );
+    }
+
+    /**
+     * Update the status of a specific record.
+     *
+     * @param int $status
+     * @return bool True on success, false on failure.
+     */
+    public function add_or_update_status($status) {
+        global $DB;
+
+        return $this->save_record($status);
+    }
+
+    /**
+     * Update the status of a specific record.
+     *
+     * @param string $note
+     * @return bool True on success, false on failure.
+     */
+    public function add_or_update_notes($note) {
+        global $DB;
+
+        return $this->save_record(null, $note);
+    }
+}
---
+++ tests/optiondates/optiondates_test.php
@@ -0,0 +1,150 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tests for booking option events.
+ *
+ * @package mod_booking
+ * @category test
+ * @copyright 2023 Wunderbyte GmbH <info@wunderbyte.at>
+ * @author 2017 Andraž Prinčič
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_booking;
+
+use advanced_testcase;
+use mod_booking\local\optiondates\optiondate;
+
+defined('MOODLE_INTERNAL') || die();
+global $CFG;
+require_once($CFG->dirroot . '/mod/booking/lib.php');
+
+/**
+ * PHPUnit test case for the class.
+ */
+final class optiondates_test extends advanced_testcase {
+    /**
+     * Data provider for the test.
+     *
+     * @return array Test data.
+     */
+    public static function data_provider(): array {
+        return [
+            [1, 1, 1, 1, 'Test notes 1', 'Test JSON 1'],
+            [2, 2, 2, 0, 'Test notes 2', 'Test JSON 2'],
+            [3, 3, 3, 2, null, null],
+        ];
+    }
+
+    /**
+     * Test save_record and get_record methods.
+     *
+     * @covers \mod_booking/local/optiondates/optiondate
+     * @dataProvider data_provider
+     */
+    public function test_save_and_get_record(
+        $userid,
+        $optiondateid,
+        $optionid,
+        $status,
+        $notes,
+        $json
+    ): void {
+        global $DB;
+        $this->resetAfterTest(true);
+
+        $manager = new optiondate($userid, $optiondateid, $optionid);
+        $manager->save_record($status, $notes, $json);
+
+        $record = $manager->get_record();
+
+        $this->assertNotEmpty($record);
+        $this->assertEquals($userid, $record->userid);
+        $this->assertEquals($optiondateid, $record->optiondateid);
+        $this->assertEquals($optionid, $record->optionid);
+        $this->assertEquals($status, $record->status);
+        $this->assertEquals($notes, $record->notes);
+        $this->assertEquals($json, $record->json);
+    }
+
+    /**
+     * Test delete_record method.
+     *
+     * @covers \mod_booking/local/optiondates/optiondate
+     * @dataProvider data_provider
+     */
+    public function test_delete_record(
+        $userid,
+        $optiondateid,
+        $optionid,
+        $status,
+        $notes,
+        $json
+    ): void {
+        $this->resetAfterTest(true);
+
+        $manager = new optiondate($userid, $optiondateid, $optionid);
+        $manager->save_record($status, $notes, $json);
+
+        $this->assertNotEmpty($manager->get_record());
+
+        $manager->delete_record();
+        $this->assertEmpty($manager->get_record());
+    }
+
+    /**
+     * Test delete_record method.
+     *
+     * @covers \mod_booking/local/optiondates/optiondate
+     * @dataProvider data_provider
+     */
+    public function test_update_record(
+        $userid,
+        $optiondateid,
+        $optionid,
+        $status,
+        $notes,
+        $json
+    ): void {
+        $this->resetAfterTest(true);
+
+        $manager = new optiondate($userid, $optiondateid, $optionid);
+        $manager->add_or_update_status($status);
+
+        $this->assertNotEmpty($manager->get_record());
+
+        $newstatus = $status + 1;
+        $manager->add_or_update_status($newstatus);
+
+        $record = $manager->get_record();
+        $this->assertEquals($newstatus, $record->status);
+        $this->assertEmpty($record->notes);
+
+        $manager->add_or_update_notes($notes);
+        $record = $manager->get_record();
+        $this->assertEquals($notes, $record->notes);
+
+        $newnotes = "$notes x";
+        $manager->add_or_update_notes($newnotes);
+
+        $record = $manager->get_record();
+        // Status is still the same.
+        $this->assertEquals($newstatus, $record->status);
+
+        $this->assertEquals($newnotes, $record->notes);
+    }
+}
