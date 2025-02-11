0529f937be64803b7159b03bd014286dcf421512
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2025-02-11 14:21:05 +0100

    Improvement: Add setting if limitation is applied for given instance only #818

--- lib.php
+++ lib.php
@@ -740,6 +740,9 @@ function booking_add_instance($booking) {
     if (isset($booking->billboardtext)) {
         // This will store the correct JSON to $optionvalues->json.
         booking::add_data_to_json($booking, "billboardtext", $booking->billboardtext);
+	}
+    if (isset($booking->maxoptionsfromcategory)) {
+        booking::add_data_to_json($booking, 'maxoptionsfromcategory', $booking->maxoptionsfromcategory);
     }
 
     // If no policy was entered, we still have to check for HTML tags.
@@ -1028,6 +1031,7 @@ function booking_update_instance($booking) {
 
     if (empty($booking->maxoptionsfromcategorycount)) {
         booking::remove_key_from_json($booking, "maxoptionsfromcategory");
+        booking::remove_key_from_json($booking, "maxoptionsfrominstance");
     } else if (!empty($booking->maxoptionsfromcategoryvalue)) {
         $value = json_encode(
             [
@@ -1035,6 +1039,7 @@ function booking_update_instance($booking) {
             ]
         );
         booking::add_data_to_json($booking, "maxoptionsfromcategory", $value);
+        booking::add_data_to_json($booking, "maxoptionsfrominstance", $booking->maxoptionsfrominstance);
     }
 
     // Update, delete or insert answers.
