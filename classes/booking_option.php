0f1647bbe3b709a6a8ce9e1beade746c44329b7b
David Ala <davidala@Mac.dhw>
2025-03-20 10:12:41 +0100

    Improvement: added bookingoption moved to history table #872

--- classes/booking_option.php
+++ classes/booking_option.php
@@ -2108,9 +2108,8 @@ class booking_option {
                 'presenceold' => $presenceold,
                 'presencenew' => $presencestatus,
             ];
-            $json = json_encode($presencechange);
 
-            self::booking_history_insert($status, $answerid, $optionid, $bookingid, $userid, $json);
+            self::booking_history_insert($status, $answerid, $optionid, $bookingid, $userid, $presencechange);
             $userdata->status = $presencestatus;
 
             $coursecontext = \context_course::instance($COURSE->id);
@@ -4086,6 +4085,7 @@ class booking_option {
      * @param int $optionid
      * @param int $bookingid
      * @param int $userid
+     * @param array $additionalinfos
      *
      * @return int The id of the record inserted.
      *
@@ -4096,9 +4096,10 @@ class booking_option {
         int $optionid = 0,
         int $bookingid = 0,
         int $userid = 0,
-        string $json = '',
+        array $additionalinfos = [],
     ): int {
         global $DB, $USER;
+        $json = json_encode($additionalinfos, true);
 
         $data = (object) [
             'status' => $status,
