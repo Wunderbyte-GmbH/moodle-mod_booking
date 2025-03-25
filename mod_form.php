a9508394486785c668bcfe1864d7fb5d3ed46160
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2025-03-25 10:19:45 +0100

    Bugfix: Make sure variable is declared in mod_form

--- mod_form.php
+++ mod_form.php
@@ -604,8 +604,8 @@ class mod_booking_mod_form extends moodleform_mod {
 
         // Custom fields to be shown on detail page (optionview.php).
         $customfields = booking_handler::get_customfields();
+        $customfieldshortnames = [];
         if (!empty($customfields)) {
-            $customfieldshortnames = [];
             foreach ($customfields as $cf) {
                 $name = format_string($cf->name);
                 $customfieldshortnames[$cf->shortname] = "$name ($cf->shortname)";
