59d811dab9593a6cb6cbba3ff71edf34d4f6e682
Bernhard Fischer <bernhard.johannes.fischer@gmail.com>
2025-05-19 14:47:59 +0200

    First steps for new subplings (bookingextensions).

---
+++ bookingextensions/evasys/lang/en/bookingextension_evasys.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains language strings for the subplugin.
+ *
+ * @package     wbreport_egbooking
+ * @copyright   2023 Georg Maißer <info@wunderbyte.at>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['pluginname'] = 'EG Group: Booking report';
+$string['reportidentifier'] = 'egbooking';
+$string['description'] = 'In this report you get an overview of booked and cancelled online courses of your users.';
+
+// General strings.
+$string['complcount'] = 'Completed';
+$string['infotext:tableheader'] = $string['description'];
+$string['pbl'] = 'PBL';
+$string['pp'] = 'In partner program';
+$string['tenant'] = 'Tenant';
+$string['ispartner'] = 'Is partner admin';
+$string['countbooked'] = 'No. booked online courses';
+$string['bookedoptions'] = 'Booked online courses';
+$string['countcanceled'] = 'No. canceled courses';
+$string['canceledoptions'] = 'Canceled online courses';
