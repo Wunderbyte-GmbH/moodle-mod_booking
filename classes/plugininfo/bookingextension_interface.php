241baf5d20e78e079bc1ba27ff2dac0cf1e6ae1e
Bernhard Fischer <bernhard.johannes.fischer@gmail.com>
2025-05-21 15:10:54 +0200

    Preparations for booking extensions.

---
+++ classes/plugininfo/bookingextension_interface.php
@@ -0,0 +1,40 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Interface for a single booking extension.
+ *
+ * All booking extensions must extend this class.
+ *
+ * @package     mod_booking
+ * @copyright   2025 Wunderbyte GmbH
+ * @author      Bernhard Fischer-Sengseis
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+namespace mod_booking\plugininfo;
+
+/**
+ * Interface for a single booking extension.
+ *
+ * All booking extensions must extend this class.
+ *
+ * @package     mod_booking
+ * @copyright   2025 Wunderbyte GmbH
+ * @author      Bernhard Fischer-Sengseis
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+interface bookingextension_interface {
+}
