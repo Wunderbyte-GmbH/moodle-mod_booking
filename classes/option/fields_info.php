781bea83822a9b8c9fc015d77051dfc64a536b6b
Bernhard Fischer <bernhard.johannes.fischer@gmail.com>
2025-07-30 12:24:32 +0200

    Code quality: commented out not yet used header of shared places section

--- classes/option/fields_info.php
+++ classes/option/fields_info.php
@@ -149,9 +149,11 @@ class fields_info {
             case MOD_BOOKING_HEADER_DATES:
                 $headericon = '<i class="fa fa-fw fa-calendar" aria-hidden="true"></i>';
                 break;
-            case MOD_BOOKING_HEADER_SHAREDPLACES:
+            // Todo: Add this when we integrate shared places.
+            // phpcs:ignore Squiz.PHP.CommentedOutCode.Found
+            /* case MOD_BOOKING_HEADER_SHAREDPLACES:
                 $headericon = '<i class="fa fa-fw fa-share-alt" aria-hidden="true"></i>';
-                break;
+                break; */
             case MOD_BOOKING_HEADER_CERTIFICATE:
                 $headericon = '<i class="fa fa-fw fa-certificate" aria-hidden="true"></i>';
                 break;
