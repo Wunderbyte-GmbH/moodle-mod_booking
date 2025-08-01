9fd748c1f7a4088b9982069b9935a845976bd64d
Bernhard Fischer <bernhard.johannes.fischer@gmail.com>
2025-08-01 14:52:59 +0200

    Version (2025080100)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025073100;
+$plugin->version = 2025080100;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->release = '8.14.13';
 $plugin->maturity = MATURITY_STABLE;
