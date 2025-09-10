43ca2a65ba4daeaa2cb8c0872d131b0504d198c5
Bernhard Fischer <bernhard.johannes.fischer@gmail.com>
2025-09-11 00:04:49 +0200

    Fix strings for new hascompetency condition.

--- lang/de/booking.php
+++ lang/de/booking.php
@@ -281,12 +281,12 @@ $string['bocondfullybookedoverridenotavailable'] = 'Ausgebucht';
 $string['bocondhascompetency'] = 'Benutzer:in hat bestimmte Kompetenzen';
 $string['bocondhascompetencyavailable'] = 'Buchen';
 $string['bocondhascompetencyfullavailable'] = 'Buchen möglich';
-$string['bocondhascompetencyfullnotavailable'] = 'Nur Benutzer:innen, die die folgende(n) Kompetenz(en) haben, dürfen buchen: {$a}
+$string['bocondhascompetencyfullnotavailable'] = 'Nur Benutzer:innen, die mind. eine der folgenden Kompetenzen haben, dürfen buchen: {$a}
     <br>Sie haben aber das Recht dennoch zu buchen.';
-$string['bocondhascompetencyfullnotavailableand'] = 'Nur Benutzer:innen, die die folgende(n) Kompetenz(en) haben, dürfen buchen: {$a}
+$string['bocondhascompetencyfullnotavailableand'] = 'Nur Benutzer:innen, die alle folgenden Kompetenzen haben, dürfen buchen: {$a}
 <br>Sie haben aber das Recht dennoch zu buchen.';
 $string['bocondhascompetencynotavailable'] = 'Buchen nicht möglich, da Sie mindestens eine der folgenden Kompetenzen nicht haben: {$a}';
-$string['bocondhascompetencynotavailableand'] = 'Buchen nicht möglich, da Sie nicht alle der folgenden Kompetenzen haben: {$a}';
+$string['bocondhascompetencynotavailableand'] = 'Buchen nicht möglich, da Sie nicht alle folgenden Kompetenzen haben: {$a}';
 $string['bocondinstanceavailability'] = 'instanceavailability: Voraussetzungen der Instanz';
 $string['bocondinstanceavailabilityavailable'] = 'Buchen';
 $string['bocondinstanceavailabilityfullavailable'] = 'Buchen möglich';
