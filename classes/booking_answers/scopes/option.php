5c5c526634e5bfabb53d35e2ee4757b03e1ff70f
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2025-09-11 15:04:15 +0200

    Delete useless functions Wunderbyte-GmbH/Moodle-local_taskflow#105

--- classes/booking_answers/scopes/option.php
+++ classes/booking_answers/scopes/option.php
@@ -394,27 +394,4 @@ class option extends scope_base {
         }
     }
 
-    /**
-     * Returns the text to be shown as an extra description for the scope.
-     *
-     * This function returns an array of objects. Each object contains the properties:
-     *  - 'text'
-     *  - 'class'
-     *  - 'link'
-     *
-     * Example:
-     * [
-     *     {
-     *         'text': 'any text',
-     *         'class': 'any class',
-     *         'link': 'a valid link or an empty string'
-     *     }
-     * ]
-     *
-     * @param int $statusparam
-     * @return array Array of objects containing 'text', 'class', and 'link' properties.
-     */
-    public function get_additional_texts(int $statusparam): array {
-        return [];
-    }
 }
