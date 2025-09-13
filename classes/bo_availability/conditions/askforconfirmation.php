fc44120a08a2916ad1ce647a91069f96b5f082de
Georg Maißer <georg.maisser@wunderbyte.at>
2025-09-13 11:39:46 +0200

    Performance: Don't render not needed descriptions

--- classes/bo_availability/conditions/askforconfirmation.php
+++ classes/bo_availability/conditions/askforconfirmation.php
@@ -198,7 +198,7 @@ class askforconfirmation implements bo_condition {
 
         $isavailable = $this->is_available($settings, $userid, $not);
 
-        $description = $this->get_description_string($isavailable, $full, $settings);
+        $description = !$isavailable ? $this->get_description_string($isavailable, $full, $settings) : '';
 
         return [$isavailable, $description, MOD_BOOKING_BO_PREPAGE_BOOK, MOD_BOOKING_BO_BUTTON_MYBUTTON];
     }
