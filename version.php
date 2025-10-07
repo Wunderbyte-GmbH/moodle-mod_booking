e1fb6f4c9afc21a5ab2a5d7a39fbff73d9c8c2e2
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2025-10-07 13:04:17 +0200

    Version 8.17.6 (2025100700)

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025100201;
+$plugin->version = 2025100700;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
-$plugin->release = '8.17.4';
+$plugin->release = '8.17.6';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'mod_booking';
 $plugin->supported = [401, 405];
