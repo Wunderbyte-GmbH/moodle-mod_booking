10bb6ab35f8eb0c7736b941ee0c505ff8608b655
Georg Maißer <georg.maisser@wunderbyte.at>
2025-11-10 10:12:52 +0100

    Version (2025111000)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025110300;
+$plugin->version = 2025111000;
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->release = '8.18.0';
 $plugin->maturity = MATURITY_STABLE;
