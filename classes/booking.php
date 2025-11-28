07ce5b330eeb6728e9a100b5d93af4e34cc7c530
mm-poustini <mmp1368@gmail.com>
2025-11-28 18:48:46 +0100

    Bugfix: Check if full text search columns contains the custom fields. Wunderbyte-GmbH/moodle-local_wunderbyte_table#102

--- classes/booking.php
+++ classes/booking.php
@@ -2246,6 +2246,12 @@ class booking {
             if (in_array($customfield, $columns, true)) {
                 $requiredcustomfields[] = $customfield;
             }
+
+            // 5. Check full text search columns if there are any custom fields.
+            $columns = empty($tableinstance) ? [] : array_keys($tableinstance->fulltextsearchcolumns);
+            if (in_array($customfield, $columns, true)) {
+                $requiredcustomfields[] = $customfield;
+            }
         }
 
         return array_values(array_unique($requiredcustomfields));
