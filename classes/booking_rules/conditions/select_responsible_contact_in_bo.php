c8f5480e32f376a26dba49884c6d45ab2616dc45
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2025-12-03 15:35:18 +0100

    Improvement: Possibility to select responsible contact as reciever of mails from rules Wunderbyte-Gmbh/Wunderbyte-GmbH#915

---
+++ classes/booking_rules/conditions/select_responsible_contact_in_bo.php
@@ -0,0 +1,160 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_booking\booking_rules\conditions;
+
+use mod_booking\booking_rules\booking_rule_condition;
+use MoodleQuickForm;
+use stdClass;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/mod/booking/lib.php');
+
+/**
+ * Condition how to identify concerned users by matching booking option field and user profile field.
+ *
+ * @package mod_booking
+ * @copyright 2022 Wunderbyte GmbH <info@wunderbyte.at>
+ * @author Georg Maißer
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class select_responsible_contact_in_bo implements booking_rule_condition {
+    /** @var string $rulename */
+    public $conditionname = 'select_responsible_contact_in_bo';
+
+    /** @var string $conditionnamestringid Id of localized string for name of rule condition*/
+    protected $conditionnamestringid = 'selectresponsiblecontactinbo';
+
+    /** @var string $rulejson a json string for a booking rule */
+    public $rulejson = '';
+
+    /**
+     * Function to tell if a condition can be combined with a certain booking rule type.
+     * @param string $bookingruletype e.g. "rule_daysbefore" or "rule_react_on_event"
+     * @return bool true if it can be combined
+     */
+    public function can_be_combined_with_bookingruletype(string $bookingruletype): bool {
+        // This condition can currently be combined with any rule.
+        return true;
+    }
+
+    /**
+     * Load json data from DB into the object.
+     * @param stdClass $record a rule condition record from DB
+     */
+    public function set_conditiondata(stdClass $record) {
+        $this->set_conditiondata_from_json($record->rulejson);
+    }
+
+    /**
+     * Load data directly from JSON.
+     * @param string $json a json string for a booking rule
+     */
+    public function set_conditiondata_from_json(string $json) {
+        $this->rulejson = $json;
+    }
+
+    /**
+     * Only customizable functions need to return their necessary form elements.
+     *
+     * @param MoodleQuickForm $mform
+     * @param ?array $ajaxformdata
+     * @return void
+     */
+    public function add_condition_to_mform(MoodleQuickForm &$mform, ?array &$ajaxformdata = null) {
+        $mform->addElement(
+            'static',
+            'select_responsible_contact_in_bo',
+            '',
+            get_string('conditionselectresponsiblecontactinbo_desc', 'mod_booking')
+        );
+    }
+
+    /**
+     * Get the name of the rule.
+     *
+     * @param bool $localized
+     * @return string the name of the rule
+     */
+    public function get_name_of_condition($localized = true) {
+        return $localized ? get_string($this->conditionnamestringid, 'mod_booking') : $this->conditionname;
+    }
+
+    /**
+     * Save the JSON for all sendmail_daysbefore rules defined in form.
+     * @param stdClass $data form data reference
+     */
+    public function save_condition(stdClass &$data): void {
+        global $DB;
+
+        if (!isset($data->rulejson)) {
+            $jsonobject = new stdClass();
+        } else {
+            $jsonobject = json_decode($data->rulejson);
+        }
+
+        $jsonobject->conditionname = $this->conditionname;
+
+        $data->rulejson = json_encode($jsonobject);
+    }
+
+    /**
+     * Sets the rule defaults when loading the form.
+     * @param stdClass $data reference to the default values
+     * @param stdClass $record a record from booking_rules
+     */
+    public function set_defaults(stdClass &$data, stdClass $record) {
+
+        $data->bookingruleconditiontype = $this->conditionname;
+    }
+
+    /**
+     * Execute the condition.
+     * We receive an array of stdclasses with the keys optinid & cmid.
+     * @param stdClass $sql
+     * @param array $params
+     */
+    public function execute(stdClass &$sql, array &$params): void {
+
+        global $DB;
+
+        // We pass the restriction to the userid in the params.
+        // If its not 0, we add the restriction.
+        $anduserid = '';
+        if (!empty($params['userid'])) {
+            // We cannot use params twice, so we need to use userid2.
+            $params['userid2'] = $params['userid'];
+            $anduserid = "AND u.id = :userid2";
+        }
+
+        // If the select contains optiondate, we also need to include it in uniqueid.
+        if (strpos($sql->select, 'optiondate') !== false) {
+            $concat = $DB->sql_concat("bo.id", "'-'", "bod.id", "'-'", "u.id");
+        } else {
+            $concat = $DB->sql_concat("bo.id", "'-'", "u.id");
+        }
+
+        // We need the hack with uniqueid so we do not lose entries ...as the first column needs to be unique.
+        $sql->select = " $concat uniqueid, " . $sql->select;
+        $sql->select .= ", u.id userid ";
+
+        $sql->from .= " JOIN {user} u
+                            ON POSITION(',' || u.id || ',' IN ',' || bo.responsiblecontact || ',') > 0 ";
+
+        $sql->where .= " $anduserid ";
+    }
+}
