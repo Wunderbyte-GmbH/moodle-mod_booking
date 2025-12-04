e975f765512e7da1f14249799349a4a239bca884
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2025-12-04 14:28:37 +0100

    Version (2025120400)
    # Conflicts:
    #	version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025120300;
+$plugin->version = 2025120400;
 $plugin->requires = 2024100700; // Requires this Moodle version. Current: Moodle 4.5.
 $plugin->release = '9.0.0';
 $plugin->maturity = MATURITY_STABLE;
