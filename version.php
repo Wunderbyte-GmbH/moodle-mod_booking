b1f96c4e52b3763db004b8f3349fe5dca95d3bc3
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2025-12-12 13:34:26 +0100

    Version (2025111803) Temp urise delivery

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025111801;
+$plugin->version = 2025111803; // Temp URISE delivery.
 $plugin->requires = 2022112800; // Requires this Moodle version. Current: Moodle 4.1.
 $plugin->release = '8.18.3';
 $plugin->maturity = MATURITY_STABLE;
