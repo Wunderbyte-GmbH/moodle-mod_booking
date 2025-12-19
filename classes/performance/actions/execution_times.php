12ac6f77d2da0ee4fb392cea1c0816b8567784ba
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2025-12-19 16:00:44 +0100

    Improvement: Adding actions. Setup pipelines and singletons
    fix tables

---
+++ classes/performance/actions/execution_times.php
@@ -0,0 +1,81 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Handle fields for booking option.
+ *
+ * @package mod_booking
+ * @copyright 2023 Wunderbyte GmbH <info@wunderbyte.at>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace mod_booking\performance\actions;
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/mod/booking/lib.php');
+
+/**
+ * Regestry for all available actions.
+ *
+ * @copyright Wunderbyte GmbH <info@wunderbyte.at>
+ * @author Georg Maißer
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class execution_times implements performance_action_interface {
+
+    private int $times = 1;
+    public static function id(): string {
+        return 'execution_times';
+    }
+
+    public static function label(): string {
+        return 'execution_times';
+    }
+
+    public static function execution_point(): execution_point {
+        return execution_point::EXECUTION_TIMES;
+    }
+
+    public function configure(array $config): void {
+        if (isset($config['counter']) && is_numeric($config['counter'])) {
+            $this->times = max(1, (int)$config['counter']);
+        }
+    }
+
+    /** No-op: this action does not execute */
+    public function execute(): void {
+        // intentionally empty
+    }
+
+    public function get_times(): int {
+        return $this->times;
+    }
+
+    public function export_for_template(\core\output\renderer_base $renderer): array {
+        return [
+            'id' => self::id(),
+            'label' => self::label(),
+            'html'  => $renderer->render_from_template(
+            'mod_booking/performance/actions/execution_times',
+                [
+                    'id' => self::id(),
+                    'value' => $this->times,
+                ]
+            ),
+        ];
+    }
+}
