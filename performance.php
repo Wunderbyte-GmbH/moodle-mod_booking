bb92d031ecdb7a3c518e91baf2817f64343b2e6f
Georg Maißer <georg.maisser@wunderbyte.at>
2025-12-25 16:01:00 +0100

    Improvement: Add capabilities view and edit performance
    # Conflicts:
    #	db/upgrade.php

--- performance.php
+++ performance.php
@@ -1,6 +1,4 @@
 <?php
-
-use mod_booking\performance\actions\action_registry;
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -24,17 +22,19 @@ use mod_booking\performance\actions\action_registry;
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-global $DB;
 require_once(__DIR__ . '/../../config.php');
+global $DB;
 
+use mod_booking\performance\actions\action_registry;
 use mod_booking\performance\performance_renderer;
 
 // No guest autologin.
 require_login(0, false);
 
-//require_capability('mod/booking:editbookingrules', $context);
+$context = context_system::instance();
+require_capability('mod/booking:viewperformance', $context);
 
-$PAGE->set_context(context_system::instance());
+$PAGE->set_context($context);
 
 $url = new moodle_url('/mod/booking/performance.php', []);
 $PAGE->set_url($url);
