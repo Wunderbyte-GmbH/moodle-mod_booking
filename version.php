0ee357c13431a8bb864f5e8375215a9edb1d0524
Magdalena Holczik <magdalena.holczik@wunderbyte.at>
2026-01-20 09:23:08 +0100

    Version (2026011604)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026011603;
+$plugin->version = 2026011604;
 $plugin->requires = 2024100700; // Requires this Moodle version. Current: Moodle 4.5.
 $plugin->release = '9.0.7';
 $plugin->maturity = MATURITY_STABLE;
