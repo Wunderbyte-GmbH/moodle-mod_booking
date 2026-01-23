a639ff904b70b62de3f8f6475a4f03ad0859b251
David Ala <david.ala@wunderbyte.at>
2026-01-23 15:56:22 +0100

    Bugfix: Stringchange Wunderbyte-GmbH/Wunderbyte-GmbH#1151

--- classes/booking_answers/scopes/supervisorteamreduced.php
+++ classes/booking_answers/scopes/supervisorteamreduced.php
@@ -111,7 +111,7 @@ class supervisorteamreduced extends supervisorteam {
         $columns = [
             'name' => get_string('fullname', 'core'),
             'text' => get_string('bookingoptionname', 'mod_booking'),
-            'action_confirm_delete' => get_string('confirmbooking', 'mod_booking'),
+            'action_confirm_delete' => get_string('unconfirm', 'mod_booking'),
             'coursestarttime' => get_string('coursestart', 'core'),
         ];
         return $columns;
