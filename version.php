3e568a7580a29e6214d8accdad061381bea81473
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2026-02-04 12:07:48 +0100

    Version (2026020302)

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026020301;
+$plugin->version = 2026020302;
 $plugin->requires = 2024100700; // Requires this Moodle version. Current: Moodle 4.5.
 $plugin->release = '9.1.0';
 $plugin->maturity = MATURITY_STABLE;
