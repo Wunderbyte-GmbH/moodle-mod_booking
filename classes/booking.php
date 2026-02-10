8e7a35eac89d5a5ed0b08a888369a1db8cf5fac0
Georg Maißer <georg.maisser@wunderbyte.at>
2026-02-10 12:21:29 +0100

    Bugfix: Make sure users can instantiate connected options (eg previously booked) even when those are invisible

--- classes/booking.php
+++ classes/booking.php
@@ -1198,7 +1198,12 @@ class booking {
         if (!$context || !has_capability('mod/booking:canseeinvisibleoptions', $context)) {
             // If we have a direct link, we only hide totally invisible options.
             // Also, if the user has already booked and looks at her table, she should see it.
-            if (isset($where['id']) || !empty($userid)) {
+            if (isset($where['id'])) {
+                // If we get one precise settings object, we always fetch it.
+                // Accessibilities need to be handled elsewhere.
+                // This is necessary to make sure we get the object for connected availability conditions.
+                $where = " ";
+            } else if (!empty($userid)) {
                 $where = " invisible <> 1 ";
             } else {
                 // ... then only show visible options.
@@ -1253,7 +1258,12 @@ class booking {
         // Instead of "where" we return "filter". This is to support the filter functionality of wunderbyte table.
         [$select2, $from2, $filter2, $params2] = booking_option_settings::return_sql_for_teachers();
         [$select3, $from3, $filter3, $params3] = booking_option_settings::return_sql_for_imagefiles();
-        [$select4, $from4, $filter4, $params4, $conditionsql] = bo_info::return_sql_from_conditions($userid ?? 0);
+
+        // When we actually ask for one specific record, we always need to return it and don't apply where conditions.
+        // This is important because of the connected availability conditions.
+        if (empty($where['id'])) {
+            [$select4, $from4, $filter4, $params4, $conditionsql] = bo_info::return_sql_from_conditions($userid ?? 0);
+        }
 
         // The $outerfrom takes all the select from the supplementary selects.
         $outerfrom .= !empty($select1) ? ", $select1 " : '';
