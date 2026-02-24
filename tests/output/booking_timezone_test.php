145020d068bb2ddfad9a172119882a4df8db348a
mm-poustini <mmp1368@gmail.com>
2026-02-24 16:08:27 +0100

    Test: Create a new scenario to verify the expected behavior when `forcetimezone` is set to a specific timezone. Wunderbyte-GmbH/Wunderbyte-GmbH#1276

--- tests/output/booking_timezone_test.php
+++ tests/output/booking_timezone_test.php
@@ -67,6 +67,138 @@ final class booking_timezone_test extends advanced_testcase {
         set_config('timezone', 'UTC');
         set_config('forcetimezone', '99');
 
+        $data = $this->setup_environment();
+        $student1 = $data['student1'];
+        $student2 = $data['student2'];
+        $student3 = $data['student3'];
+        $student4 = $data['student4'];
+        $student5 = $data['student5'];
+        $option = $data['option'];
+        $cm = $data['cm'];
+        $timevalues = $data['timevalues'];
+
+        $table = new bookingoptions_wbtable("cmid_{$cm->id} allbookingoptionstable");
+        $table->define_cache('mod_booking', 'bookingoptionstable');
+        cache_helper::purge_all();
+
+        // Student 1 should see the showdates in CET timezone and the output should be cached with timezone abbreviation for CET.
+        $this->setUser($student1);
+        $output1 = $table->col_showdates((object)['id' => $option->id]);
+        $this->assertStringContainsString('13 March 2045, 1:00 PM (CET)', $output1);
+        $this->assertStringContainsString('3:00 PM (CET)', $output1);
+        $opening1 = $table->col_bookingopeningtime($timevalues);
+        $closing1 = $table->col_bookingclosingtime($timevalues);
+        $this->assertStringContainsString('Bookable from: 13 March 2045, 11:00 AM (CET)', $opening1);
+        $this->assertStringContainsString('Bookable until: 13 March 2045, 5:00 PM (CET)', $closing1);
+
+        // We check the cached data for student 1 if it is cached properly.
+        $lang = current_language();
+        $timezone = \core_date::get_user_timezone($student1);
+        $timezonetoken = str_replace('/', '_', $timezone);
+        $cachekey = "sessiondates{$option->id}{$lang}{$timezonetoken}";
+        $cache = \cache::make('mod_booking', 'bookingoptionstable');
+        $this->assertSame($output1, $cache->get($cachekey));
+
+         // Student 2 should see the showdates in Tehran timezone.
+        $this->setUser($student2);
+        $output2 = $table->col_showdates((object)['id' => $option->id]);
+        $this->assertStringContainsString('13 March 2045, 3:30 PM (Tehran)', $output2);
+        $this->assertStringContainsString('5:30 PM (Tehran)', $output2);
+        $this->assertStringNotContainsString('13 March 2045, 1:00 PM (CET)', $output2);
+        $opening2 = $table->col_bookingopeningtime($timevalues);
+        $closing2 = $table->col_bookingclosingtime($timevalues);
+        $this->assertStringContainsString('Bookable from: 13 March 2045, 1:30 PM (Tehran)', $opening2);
+        $this->assertStringContainsString('Bookable until: 13 March 2045, 7:30 PM (Tehran)', $closing2);
+
+        // Student 3 should see the showdates in CDT timezone.
+        $this->setUser($student3);
+        $output3 = $table->col_showdates((object)['id' => $option->id]);
+        $this->assertStringContainsString('13 March 2045, 7:00 AM (CDT)', $output3);
+        $this->assertStringContainsString('9:00 AM (CDT)', $output3);
+        $this->assertStringNotContainsString('13 March 2045, 1:00 PM (CET)', $output3);
+        $opening3 = $table->col_bookingopeningtime($timevalues);
+        $closing3 = $table->col_bookingclosingtime($timevalues);
+        $this->assertStringContainsString('Bookable from: 13 March 2045, 5:00 AM (CDT)', $opening3);
+        $this->assertStringContainsString('Bookable until: 13 March 2045, 11:00 AM (CDT)', $closing3);
+
+        // Chek if student 4 with the same timezone as student 1 gets the same cached output without timezone abbreviation.
+        $this->setUser($student4);
+        $output4 = $table->col_showdates((object)['id' => $option->id]);
+        $this->assertStringContainsString('13 March 2045, 1:00 PM (CET)', $output4);
+        $this->assertStringContainsString('3:00 PM (CET)', $output4);
+        $opening4 = $table->col_bookingopeningtime($timevalues);
+        $closing4 = $table->col_bookingclosingtime($timevalues);
+        $this->assertSame($opening1, $opening4);
+        $this->assertSame($closing1, $closing4);
+        $this->assertSame($output4, $cache->get($cachekey));
+
+        // Check that the displayed times for users with the same timezone do not include the timezone abbreviation.
+        $this->setUser($student5);
+        $output5 = $table->col_showdates((object)['id' => $option->id]);
+        $shoudcontains = "13 March 2045, 12:00 PM";
+        $this->assertStringContainsString($shoudcontains, $output5);
+        $this->assertStringNotContainsString('(', $output5);
+        $opening0 = $table->col_bookingopeningtime($timevalues);
+        $closing0 = $table->col_bookingclosingtime($timevalues);
+        $this->assertStringNotContainsString('(', $opening0);
+        $this->assertStringNotContainsString('(', $closing0);
+    }
+
+    /**
+     * Summary of test_showdates_cache_respects_forced_timezone
+     * @return void
+     */
+    public function test_showdates_cache_respects_forced_timezone(): void {
+        global $PAGE;
+
+        $data = $this->setup_environment();
+        $student1 = $data['student1'];
+        $student2 = $data['student2'];
+        $student3 = $data['student3'];
+        $student4 = $data['student4'];
+        $student5 = $data['student5'];
+        $record = $data['record'];
+        $option = $data['option'];
+        $cm = $data['cm'];
+        $timevalues = $data['timevalues'];
+
+        $this->setAdminUser();
+        set_config('forcetimezone', 'Europe/Vienna');
+
+        $tableforced = new bookingoptions_wbtable("cmid_{$cm->id} allbookingoptionstable");
+        $tableforced->define_cache('mod_booking', 'bookingoptionstable');
+        cache_helper::purge_all();
+
+        $this->setUser($student2);
+        $outputforced = $tableforced->col_showdates((object)['id' => $option->id]);
+        $this->assertStringContainsString('13 March 2045, 1:00 PM (CET)', $outputforced);
+        $this->assertStringContainsString('3:00 PM (CET)', $outputforced);
+        $this->assertStringNotContainsString('(Tehran)', $outputforced);
+        $openingforced = $tableforced->col_bookingopeningtime($timevalues);
+        $closingforced = $tableforced->col_bookingclosingtime($timevalues);
+        $this->assertStringContainsString('Bookable from: 13 March 2045, 11:00 AM (CET)', $openingforced);
+        $this->assertStringContainsString('Bookable until: 13 March 2045, 5:00 PM (CET)', $closingforced);
+
+        // Check that the displayed times for users with the same timezone do not include the timezone abbreviation.
+        $this->setUser($student5);
+        $output5 = $tableforced->col_showdates((object)['id' => $option->id]);
+        $shoudcontains = "13 March 2045, 12:00 PM";
+        $this->assertStringContainsString($shoudcontains, $output5);
+        $this->assertStringNotContainsString('(', $output5);
+        $opening0 = $tableforced->col_bookingopeningtime($timevalues);
+        $closing0 = $tableforced->col_bookingclosingtime($timevalues);
+        $this->assertStringNotContainsString('(', $opening0);
+        $this->assertStringNotContainsString('(', $closing0);
+    }
+
+    /**
+     * Initialize environment.
+     *
+     * @return array
+     */
+    public function setup_environment() {
+        global $PAGE;
+
         $course = $this->getDataGenerator()->create_course();
         $teacher = $this->getDataGenerator()->create_user([
             'username' => 'teacher1',
@@ -108,7 +240,14 @@ final class booking_timezone_test extends advanced_testcase {
             'timezone' => 'Europe/Vienna',
             'lang' => 'en',
         ]);
-
+        $student5 = $this->getDataGenerator()->create_user([
+            'username' => 'student5',
+            'firstname' => 'Student',
+            'lastname' => '5',
+            'email' => 'student5@example.com',
+            'timezone' => 'UTC',
+            'lang' => 'en',
+        ]);
 
         $this->getDataGenerator()->enrol_user($teacher->id, $course->id, 'editingteacher');
         $this->getDataGenerator()->enrol_user($student1->id, $course->id, 'student');
@@ -148,160 +287,24 @@ final class booking_timezone_test extends advanced_testcase {
         $cm = get_coursemodule_from_instance('booking', $booking->id, $course->id, false, MUST_EXIST);
         $PAGE->set_context(context_module::instance($cm->id));
 
-        cache_helper::purge_all();
-
-        $table = new bookingoptions_wbtable("cmid_{$cm->id} allbookingoptionstable");
-        $table->define_cache('mod_booking', 'bookingoptionstable');
         $timevalues = (object)[
             'id' => $option->id,
             'bookingopeningtime' => $record->bookingopeningtime,
             'bookingclosingtime' => $record->bookingclosingtime,
         ];
-        $formatdatetime = get_string('strftimedatetime', 'langconfig');
-        $formattime = get_string('strftimetime', 'langconfig');
 
-        $this->setUser($student1);
-        $output1 = $table->col_showdates((object)['id' => $option->id]);
-        $expectedstart1 = booking_format_userdate_with_timezone_abbr(
-            $record->coursestarttime_0,
-            $formatdatetime,
-            $student1
-        );
-        $expectedend1 = booking_format_userdate_with_timezone_abbr(
-            $record->courseendtime_0,
-            $formattime,
-            $student1
-        );
-        $this->assertStringContainsString($expectedstart1, $output1);
-        $this->assertStringContainsString($expectedend1, $output1);
-        $this->assertStringContainsString(' (', $expectedstart1);
-        $opening1 = $table->col_bookingopeningtime($timevalues);
-        $closing1 = $table->col_bookingclosingtime($timevalues);
-        $expectedopen1 = booking_format_userdate_with_timezone_abbr(
-            $record->bookingopeningtime,
-            $formatdatetime,
-            $student1
-        );
-        $expectedclose1 = booking_format_userdate_with_timezone_abbr(
-            $record->bookingclosingtime,
-            $formatdatetime,
-            $student1
-        );
-        $this->assertStringContainsString('Bookable from: ' . $expectedopen1, $opening1);
-        $this->assertStringContainsString('Bookable until: ' . $expectedclose1, $closing1);
-        $lang = current_language();
-        $timezone = \core_date::get_user_timezone($student1);
-        $timezonetoken = str_replace('/', '_', $timezone);
-        $cachekey = "sessiondates{$option->id}{$lang}{$timezonetoken}";
-        $cache = \cache::make('mod_booking', 'bookingoptionstable');
-        $this->assertSame($output1, $cache->get($cachekey));
-
-        $this->setUser($student2);
-        $output2 = $table->col_showdates((object)['id' => $option->id]);
-        $expectedstart2 = booking_format_userdate_with_timezone_abbr(
-            $record->coursestarttime_0,
-            $formatdatetime,
-            $student2
-        );
-        $expectedend2 = booking_format_userdate_with_timezone_abbr(
-            $record->courseendtime_0,
-            $formattime,
-            $student2
-        );
-        $this->assertStringContainsString($expectedstart2, $output2);
-        $this->assertStringContainsString($expectedend2, $output2);
-        $this->assertStringNotContainsString($expectedstart1, $output2);
-        $opening2 = $table->col_bookingopeningtime($timevalues);
-        $closing2 = $table->col_bookingclosingtime($timevalues);
-        $expectedopen2 = booking_format_userdate_with_timezone_abbr(
-            $record->bookingopeningtime,
-            $formatdatetime,
-            $student2
-        );
-        $expectedclose2 = booking_format_userdate_with_timezone_abbr(
-            $record->bookingclosingtime,
-            $formatdatetime,
-            $student2
-        );
-        $this->assertStringContainsString('Bookable from: ' . $expectedopen2, $opening2);
-        $this->assertStringContainsString('Bookable until: ' . $expectedclose2, $closing2);
-
-        $this->setUser($student3);
-        $output3 = $table->col_showdates((object)['id' => $option->id]);
-        $expectedstart3 = booking_format_userdate_with_timezone_abbr(
-            $record->coursestarttime_0,
-            $formatdatetime,
-            $student3
-        );
-        $expectedend3 = booking_format_userdate_with_timezone_abbr(
-            $record->courseendtime_0,
-            $formattime,
-            $student3
-        );
-        $this->assertStringContainsString($expectedstart3, $output3);
-        $this->assertStringContainsString($expectedend3, $output3);
-        $this->assertStringNotContainsString($expectedstart1, $output3);
-        $opening3 = $table->col_bookingopeningtime($timevalues);
-        $closing3 = $table->col_bookingclosingtime($timevalues);
-        $expectedopen3 = booking_format_userdate_with_timezone_abbr(
-            $record->bookingopeningtime,
-            $formatdatetime,
-            $student3
-        );
-        $expectedclose3 = booking_format_userdate_with_timezone_abbr(
-            $record->bookingclosingtime,
-            $formatdatetime,
-            $student3
-        );
-        $this->assertStringContainsString('Bookable from: ' . $expectedopen3, $opening3);
-        $this->assertStringContainsString('Bookable until: ' . $expectedclose3, $closing3);
-
-        $this->setUser($student4);
-        $output4 = $table->col_showdates((object)['id' => $option->id]);
-        $this->assertStringContainsString($expectedstart1, $output4);
-        $this->assertStringContainsString($expectedend1, $output4);
-        $opening4 = $table->col_bookingopeningtime($timevalues);
-        $closing4 = $table->col_bookingclosingtime($timevalues);
-        $this->assertSame($opening1, $opening4);
-        $this->assertSame($closing1, $closing4);
-        $this->assertSame($output4, $cache->get($cachekey));
-
-        $this->setAdminUser();
-        set_config('forcetimezone', 'Europe/Vienna');
-        cache_helper::purge_all();
-
-        $tableforced = new bookingoptions_wbtable("cmid_{$cm->id} allbookingoptionstable");
-        $tableforced->define_cache('mod_booking', 'bookingoptionstable');
-
-        $this->setUser($student2);
-        $outputforced = $tableforced->col_showdates((object)['id' => $option->id]);
-        $expectedforcedstart = booking_format_userdate_with_timezone_abbr(
-            $record->coursestarttime_0,
-            $formatdatetime,
-            $student2
-        );
-        $expectedforcedend = booking_format_userdate_with_timezone_abbr(
-            $record->courseendtime_0,
-            $formattime,
-            $student2
-        );
-        $this->assertStringContainsString($expectedforcedstart, $outputforced);
-        $this->assertStringContainsString($expectedforcedend, $outputforced);
-        $this->assertStringNotContainsString('(Tehran)', $outputforced);
-
-        $openingforced = $tableforced->col_bookingopeningtime($timevalues);
-        $closingforced = $tableforced->col_bookingclosingtime($timevalues);
-        $expectedopenforced = booking_format_userdate_with_timezone_abbr(
-            $record->bookingopeningtime,
-            $formatdatetime,
-            $student2
-        );
-        $expectedcloseforced = booking_format_userdate_with_timezone_abbr(
-            $record->bookingclosingtime,
-            $formatdatetime,
-            $student2
-        );
-        $this->assertStringContainsString('Bookable from: ' . $expectedopenforced, $openingforced);
-        $this->assertStringContainsString('Bookable until: ' . $expectedcloseforced, $closingforced);
+        return [
+            'course' => $course,
+            'teacher' => $teacher,
+            'student1' => $student1,
+            'student2' => $student2,
+            'student3' => $student3,
+            'student4' => $student4,
+            'student5' => $student5,
+            'record' => $record,
+            'option' => $option,
+            'cm' => $cm,
+            'timevalues' => $timevalues,
+        ];
     }
 }
