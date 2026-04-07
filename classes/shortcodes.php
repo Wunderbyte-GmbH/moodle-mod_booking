5ba168e9afcbe06504697f23d5c4e7172fda6a96
David Ala <david.ala@wunderbyte.at>
2026-04-07 15:48:54 +0200

    Improvement: adapt certificate shortcode to requirements #1343

--- classes/shortcodes.php
+++ classes/shortcodes.php
@@ -1429,10 +1429,16 @@ class shortcodes {
             $bookinganswer = singleton_service::get_instance_of_booking_answers($settings);
             $hascompleted = $bookinganswer->is_activity_completed($userid);
             if ($hascompleted) {
-                $texts[] = $settings->text;
+                $certificateteachers = [];
+                foreach ($settings->teachers as $teacher) {
+                    $certificateteachers[] = "$teacher->firstname $teacher->lastname";
+                }
+                $texts[] = $settings->text
+                . "<br>" . get_string('teachersbookingoptionsfromcondition', 'mod_booking')
+                . implode(",", $certificateteachers);
             }
         }
-        return implode("<br>", $texts);
+        return implode(get_string('delimiterbookingoptionsfromcondition', 'mod_booking'), $texts);
     }
 
     /**
