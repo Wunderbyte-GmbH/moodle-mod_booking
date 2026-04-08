944a7ebcf7bb56bf2f47ef0545076d41d2d5b49b
Bernhard Fischer <bernhard.fischer@wunderbyte.at>
2026-04-08 16:01:32 +0200

    Version 9.3.0 (2026040800)

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026040201;
+$plugin->version = 2026040800;
 $plugin->requires = 2024100700; // Requires this Moodle version. Current: Moodle 4.5.
-$plugin->release = '9.2.1';
+$plugin->release = '9.3.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'mod_booking';
 $plugin->supported = [405, 501];
