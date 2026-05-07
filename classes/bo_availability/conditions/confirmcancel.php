9ba00cbcb143dc12a769a16d1a09431fd84cb6dc
Andrii Semenets <semteacher@gmail.com>
2026-05-07 20:06:31 +0300

    bugfix for: confirmcancel::is_available() hides the confirmation condition for priced options in most “booked” cases (https://github.com/Wunderbyte-GmbH/Wunderbyte-GmbH/issues/1616)

--- classes/bo_availability/conditions/confirmcancel.php
+++ classes/bo_availability/conditions/confirmcancel.php
@@ -30,6 +30,7 @@ use mod_booking\bo_availability\bo_info;
 use mod_booking\booking_option_settings;
 use mod_booking\price;
 use mod_booking\singleton_service;
+use local_shopping_cart\shopping_cart_history;
 use MoodleQuickForm;
 
 defined('MOODLE_INTERNAL') || die();
@@ -130,10 +131,15 @@ class confirmcancel implements bo_condition {
             if (
                 $isavailable
                 && empty((float)($price['price'] ?? 0))
-                && empty(get_config('booking', 'displayemptyprice'))
             ) {
+                if (empty(get_config('booking', 'displayemptyprice'))) {
                 // We might want to override this, if there is a zero price.
-                $isavailable = false;
+                    $isavailable = false;
+                } else {
+                   if (isset($bookinginformation['iambooked']) && !self::has_shopping_cart_history_entry($settings->id, $userid)) {
+                        $isavailable = false;
+                    } 
+                }
             }
         }
 
@@ -169,6 +175,22 @@ class confirmcancel implements bo_condition {
         }
 
         return $isavailable;
+    }
+
+    /**
+     * Check whether this booking option was purchased via local shopping cart.
+     *
+     * @param int $optionid
+     * @param int $userid
+     * @return bool
+     */
+    private static function has_shopping_cart_history_entry(int $optionid, int $userid): bool {
+        if (!class_exists('local_shopping_cart\\shopping_cart_history')) {
+            return false;
+        }
+
+        $historyitem = shopping_cart_history::get_most_recent_historyitem('mod_booking', 'option', $optionid, $userid);
+        return !empty($historyitem->id);
     }
 
     /**
