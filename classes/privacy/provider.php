384bdc58f42fc995ef71478dd35b8b7281b45744
Wunderbyte GmbH <info@wunderbyte.at>
2026-07-09 11:21:08 +0200

    Linting

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -50,8 +50,8 @@ use stdClass;
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements
-    \core_privacy\local\request\core_userlist_provider,
     \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\core_userlist_provider,
     \core_privacy\local\request\plugin\provider {
     /**
      * Return the fields which contain personal data.
