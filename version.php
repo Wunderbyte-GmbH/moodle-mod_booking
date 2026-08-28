bc9df3e60b49214a441d64eddcdf5ea4b932d948
David Ala <david.ala@wunderbyte.at>
2026-08-28 12:06:40 +0200

    Version 9.7.8 (2026082800)

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026082002;
+$plugin->version = 2026082800;
 $plugin->requires = 2024100700; // Requires this Moodle version. Current: Moodle 4.5.
-$plugin->release = '9.7.6.2';
+$plugin->release = '9.7.8';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'mod_booking';
 $plugin->supported = [405, 502];
