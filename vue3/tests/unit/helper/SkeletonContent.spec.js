9c7beba2ca9b0b693b3e690555c4b1f4606379dc
jacob.viertel@wunderbyte.at <jacob.viertel@wunderbyte.at>
2026-09-01 09:28:42 +0200

    GH-2104: Bugfix: Added boilerplate headers

--- vue3/tests/unit/helper/SkeletonContent.spec.js
+++ vue3/tests/unit/helper/SkeletonContent.spec.js
@@ -1,3 +1,26 @@
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit tests for the SkeletonContent Vue component.
+ *
+ * @package    mod_booking
+ * @copyright  2024 Wunderbyte GmbH <info@wunderbyte.at>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 import { mount } from '@vue/test-utils';
 import SkeletonContent from '../../../components/helper/SkeletonContent.vue';
 
