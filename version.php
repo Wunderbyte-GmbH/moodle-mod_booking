ad9af8d177acbe4f1482b2922d70cc5efe8b3339
Georg Maisser <georg.maisser@wunderbyte.at>
2026-09-10 13:35:03 +0200

    Version 9.7.10 (2026090301)

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026090300;
+$plugin->version = 2026090301;
 $plugin->requires = 2024100700; // Requires this Moodle version. Current: Moodle 4.5.
-$plugin->release = '9.7.9';
+$plugin->release = '9.7.10';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'mod_booking';
 $plugin->supported = [405, 502];
